32efaf687d3ef344ce7fb01e6f3a58da9def65d5
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-02-18 19:00:30 +0900

    [Silver III] Title: 소수 구하기, Time: 80 ms, Memory: 43100 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -2,26 +2,14 @@
 
 import sys
 
-# n보다 작거나 같은 소수들의 리스트를 반환하는 함수
-def get_primes(n):
-    # 0과 1은 소수가 아니므로 False로 초기화
-    is_prime = [False, False] + [True] * (n-1)
-    primes = []
-    for i in range(2, n+1):
-        if is_prime[i]:
-            primes.append(i)
-            # i의 배수들을 모두 소수가 아닌 것으로 표시
-            for j in range(i*2, n+1, i):
-                is_prime[j] = False
-    return primes
+M, N = map(int, sys.stdin.readline().split())
 
-# 입력받은 두 수의 범위를 저장
-m, n = map(int, sys.stdin.readline().split())
+eratosthenes = [False] + [True] * ((N - 1) // 2)
 
-# n보다 작거나 같은 소수들의 리스트를 구함
-primes = get_primes(n)
+for i in range(1, int(N ** 0.5)):
+    if eratosthenes[i]:
+        eratosthenes[2 * i * (i + 1)::i * 2 + 1] = [False] * ((((N + 1) // 2) - i * i * 2) // (i * 2 + 1))
 
-# m부터 n까지의 수 중에서 소수인 수를 출력
-for p in primes:
-    if p >= m:
-        print(p)
+if M <= 2:
+    sys.stdout.write("2\n")
+sys.stdout.write('\n'.join([f'{x}' for x, val in zip(range(M+(M & 1 == 0), N + 1, 2), eratosthenes[M // 2:]) if val]))
