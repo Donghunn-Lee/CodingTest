da33bb003244dae83f79ed093942cd5429592153
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-02-18 13:50:31 +0900

    [Bronze I] Title: 최소공배수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,39 +1,13 @@
-# 최소공배수
+import sys
+input = sys.stdin.readline
 
 T = int(input())
-ans = []
-
-def lcm(a, b):
-    n = 2
-    tmp = []
-    result = 1
-    
-    if b == 1:
-        return a
-    
-    while n <= b:
-        if a % n == 0 and b % n == 0:
-            a //= n
-            b //= n
-            tmp.append(n)
-        else :
-            n += 1
-    
-    if tmp:
-        for i in tmp:
-            result *= i
-        return result * a * b
-    else:
-        return a * b
-
-
 
 for i in range(T):
-    A, B = map(int, input().split())
-    if A > B:
-        ans.append(lcm(A, B))
-    else:
-        ans.append(lcm(B, A))
+    a, b = map(int, input().split())
+    aa, bb = a, b
 
-for i in ans:
-    print(i)
+    while a % b != 0:
+        a, b = b, a % b
+
+    print(aa * bb // b)
