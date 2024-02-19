556cb7c91db25dd3f415608702954b48c976f778
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-02-19 18:38:36 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 772 ms, Memory: 130868 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -3,13 +3,13 @@ import sys
 input = sys.stdin.readline
 max = 1000000
 primes = [True for _ in range(max)]
-# ans = []
+ans = []
 
 for i in range(2, int(max ** 0.5) + 1):
     if primes[i]:
         for j in range(i*2, max, i):
             primes[j] = False
-            
+
 while True:
     N = int(input())
     b = 0
@@ -19,10 +19,11 @@ while True:
     for a in range(3, N):
         if primes[a]:
             if primes[N - a]:
-                print(f'{N} = {a} + {N - a}')
+                ans.append(f'{N} = {a} + {N - a}')
                 b = 1
                 break
         
     if b == 0:
-        print('Goldbach\'s conjecture is wrong.')
+        ans.append('Goldbach\'s conjecture is wrong.')
 
+print('\n'.join(ans))
