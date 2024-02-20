86a319e29ca4bf904e4d0661e481efeff469e87a
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-02-20 21:26:46 +0900

    [Silver III] Title: 1로 만들기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,17 +1,10 @@
-# 1로 만들기
-
-N = int(input())
-
-def DP(n):
-    count = [0] * (n + 1)
-
-    for i in range(2, n + 1):
-        count[i] = count[i - 1] + 1
-        if i % 3 == 0:
-            count[i] = min(count[i], count[i // 3] + 1)
-        if i % 2 == 0:
-            count[i] = min(count[i], count[i // 2] + 1)
-
-    return count[n]
-
-print(DP(N))
+# 너무 참신해서 저장
+import sys
+d = {1: 0, 2: 1}
+def s(n: int) -> int:
+    if n in d:
+        return d[n]
+    t = 1 + min(s(n // 3) + n % 3, s(n // 2) + n % 2)
+    d[n] = t
+    return t
+print(s(int(sys.stdin.readline())))
