4bcd1b420ff9f1db555a8b052cb3fca6d4361e86
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-02-20 21:41:30 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,10 +1,14 @@
-# 너무 참신해서 저장
+# 1로 만들기
+
 import sys
+
 d = {1: 0, 2: 1}
+
 def s(n: int) -> int:
     if n in d:
         return d[n]
     t = 1 + min(s(n // 3) + n % 3, s(n // 2) + n % 2)
     d[n] = t
     return t
+
 print(s(int(sys.stdin.readline())))
