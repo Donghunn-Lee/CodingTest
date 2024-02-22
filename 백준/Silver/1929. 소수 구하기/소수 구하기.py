3f15074edbcc1e0f153f06ca4405928edb451ba2
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-02-22 22:38:19 +0900

    [Silver III] Title: 소수 구하기, Time: 988 ms, Memory: 111504 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,15 +1,17 @@
 #소수 구하기
 
-import sys
+M, N = map(int, input().split())
+primes = []
 
-M, N = map(int, sys.stdin.readline().split())
+def calPrimeNumber():
+    for i in range(M, N + 1):
+        if i != 1:
+            for j in range(2, int(i ** 0.5) + 1):
+                if i % j == 0:
+                    break
+        
+            else:
+                print(i)
+                    
 
-eratosthenes = [False] + [True] * ((N - 1) // 2)
-
-for i in range(1, int(N ** 0.5)):
-    if eratosthenes[i]:
-        eratosthenes[2 * i * (i + 1)::i * 2 + 1] = [False] * ((((N + 1) // 2) - i * i * 2) // (i * 2 + 1))
-
-if M <= 2:
-    sys.stdout.write("2\n")
-sys.stdout.write('\n'.join([f'{x}' for x, val in zip(range(M+(M & 1 == 0), N + 1, 2), eratosthenes[M // 2:]) if val]))
+calPrimeNumber()
