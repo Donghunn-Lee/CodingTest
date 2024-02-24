f61e78ce17ab0fd3c8de626c47228b99f5574513
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-02-24 21:55:28 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 336 ms, Memory: 50332 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
@@ -0,0 +1,18 @@
+# 1, 2, 3 더하기 5
+
+T = int(input())
+nums = [[0] * 3 for _ in range(100001)]
+
+nums[1] = [1, 0, 0]
+nums[2] = [0, 1, 0]
+nums[3] = [1, 1, 1]
+
+
+for i in range(4, 100001):
+    nums[i][0] = (nums[i - 1][1] + nums[i - 1][2]) % 1000000009
+    nums[i][1] = (nums[i - 2][0] + nums[i - 2][2]) % 1000000009
+    nums[i][2] = (nums[i - 3][0] + nums[i - 3][1]) % 1000000009
+
+for _ in range(T):
+    N = int(input())
+    print(sum(nums[N]) % 1000000009)
