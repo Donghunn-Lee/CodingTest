a50f8f0b3e09eee0912dd315b288e5dda303dd4b
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-03-04 15:27:59 +0900

    [Gold V] Title: ABCDE, Time: 704 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,26 +1,28 @@
 # ABCED
-import copy
+import sys
+input = sys.stdin.readline
 
-# 현재 확인된 친구 관계 리스트와 조사할 다음 대상을 입력
-# 깊은복사로 리스트를 넘겨서 아직 조사하지 않은 대상을 찾아가며 추가하는 함수
-# 확인된 관계가 5명인 경우 result에 True를 할당하고 종료
-def dfs(c, n, l):
+# 전체 관계도와 조사할 대상, 확인된 친구관계, 현재 명 수 를 입력
+# visited를 통해 현재 루트에서 대상의 조사 확인 여부를 관리함
+def dfs(rel_map, start, visited, cnt):
     global result
-    if result or l == 5:
-        result = True
-        return
+    if cnt == 5:
+        print(1)
+        sys.exit(0)
     
-    for j in relationship[n]:
-        connected = copy.deepcopy(c)
-        if j not in connected:
-            connected.append(j)
-            dfs(connected, j, l + 1)
+    for i in rel_map[start]:
+        if not visited[i]:
+            visited[i] = True
+            dfs(rel_map, i, visited, cnt + 1)
+            visited[i] = False
+        
 
 # 메인에서는 입력된 명수만큼의 빈 리스트를 가진 리스트를 생성
 # rel[0]에 0의 친구들을 모두 입력하는 방식으로 이차원 리스트 relationship을 생성
 if __name__ == '__main__':
     N, M = map(int, input().split())
     relationship = [[] for _ in range(N)]
+    visited = [False] * N
     result = False
     
     for i in range(M):
@@ -30,6 +32,7 @@ if __name__ == '__main__':
     
     # 시간 단축을 위해 친구 관계가 적은 사람부터 조사하기 위해 order 리스트 생성
     # 친구 관계가 적을수록 해당 사람이 ABCDE 중 A 또는 E일 가능성이 높기 때문
+    
     order = []
     for i in range(N):
         order.append([i, len(relationship[i])])
@@ -38,11 +41,8 @@ if __name__ == '__main__':
     # ABCDE중 시작인 A를 0번 부터 N - 1 번까지 각각 대입
     # dfs에서 i번째를 A로 삼아 나머지 친구들을 계산함
     for i, j in order:
-        if result:
-            print(1)
-            break
-        else:
-            dfs([i], i, 1)
-
-    if not result:
-        print(0)
+        visited[i] = True
+        dfs(relationship, i, visited, 1)
+        visited[i] = False
+    
+    print(0)
