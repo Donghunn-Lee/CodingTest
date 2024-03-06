cd264e27d80c2714a6d56ce0a1813dc537dfd211
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-03-06 19:48:15 +0900

    [Gold III] Title: 오등큰수, Time: 500 ms, Memory: 262928 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.py
+++ 백준/Gold/17299. 오등큰수/오등큰수.py
@@ -6,7 +6,7 @@ input = sys.stdin.readline
 # 값을 그대로 사용하는 대신 원소의 빈도수로 계산하면 간단함
 def cal_NGE(seq, fre):
     result = [-1 for _ in range(N)]
-    stack = [[0, fre[seq[0]]]]
+    stack = []
 
     # 수열의 인덱스와 값을 사용해 빈도수를 비교
     # 최근 빈도수보다 지금 j의 빈도수가 더 적거나 같을 경우 스택에 쌓음
@@ -24,14 +24,9 @@ def cal_NGE(seq, fre):
 if __name__ == "__main__":
     N = int(input())
     A = list(map(int, input().split()))
-    tmp = set(A)
-    F = dict()
 
-    # 집합으로 중복을 없앤 후, 이것을 키로 딕셔너리를 생성
-    # count를 사용하면 O(N^2) 인 반면 이 방법이면 O(2N)으로 빈도수를 계산할 수 있음
-    for i in tmp:
-        F[i] = 0
-
+    # 빈도수를 저장할 리스트 생성
+    F = [0] * 1000001
     for i in A:
         F[i] += 1
     
