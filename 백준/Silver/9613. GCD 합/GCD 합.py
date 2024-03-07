f327802e8ff3d574b288a9486d4baccaee269897
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-03-07 21:41:50 +0900

    [Silver IV] Title: GCD 합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9613. GCD 합/GCD 합.py
@@ -0,0 +1,33 @@
+# GCD 합
+import sys
+input = sys.stdin.readline
+
+def cal_gcd(a, b):
+    while a % b != 0:
+        a, b = b, a % b
+
+    return b
+
+def sum_gcd(seq):
+    tmp_sum = 0
+
+    for i in range(len(seq)):
+        for j in range(i + 1, len(seq)):
+            tmp_sum += cal_gcd(max(seq[i], seq[j]), min(seq[i], seq[j]))
+    
+    return tmp_sum
+
+        
+
+if __name__ == "__main__":
+    t = int(input())
+    ans = []
+
+    for i in range(t):
+        n, *seq = map(int, input().split())
+        ans.append(sum_gcd(seq))
+    
+    print('\n'.join(map(str, ans)))
+
+
+    
