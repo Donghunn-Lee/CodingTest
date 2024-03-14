cc6b8ad1351de08109bcf603db1ac2bbbc538ec7
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-03-14 21:17:51 +0900

    [Silver II] Title: 최소 힙, Time: 108 ms, Memory: 40900 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,23 @@
+# 최소 힙
+
+import sys, heapq
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    N = int(input())
+    heap = []
+    ans = []
+
+    for i in range(N):
+        x = int(input())
+
+        if x == 0:
+            if heap:
+                ans.append(heapq.heappop(heap))
+            else:
+                ans.append(0)
+        else:
+            heapq.heappush(heap, x)
+    
+    print("\n".join(map(str, ans)))
+        
