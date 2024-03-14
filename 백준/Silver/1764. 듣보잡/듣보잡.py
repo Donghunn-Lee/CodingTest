c4519f78c3043fb0f79739c8bcb169acb6bca3f8
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-03-14 19:00:19 +0900

    [Silver IV] Title: 듣보잡, Time: 172 ms, Memory: 119372 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -7,8 +7,7 @@ if __name__ == "__main__":
     N, M = map(int, input().split())
     unheard = {input().strip() for _ in range(N)}
     never_see = {input().strip() for _ in range(M)}
-    ans = list(unheard & never_see)
-    
-    ans.sort()
+    ans = sorted(list(unheard & never_see))
+
     print(len(ans))
-    print('\n'.join(ans))
+    sys.stdout.write('\n'.join(ans))
