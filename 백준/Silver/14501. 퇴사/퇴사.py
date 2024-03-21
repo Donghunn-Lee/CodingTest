b5f0ee117cd5f0e42c56be6d976709e2fdb9fc2f
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-03-21 12:33:29 +0900

    [Silver III] Title: 퇴사, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,20 @@
+# 퇴사
+
+import sys
+input = sys.stdin.readline
+
+def best_schedule(cl):
+    dp = [0] * (N + 2)
+    
+
+    for i, (t, p) in enumerate(cl):
+        if i + t <= N:
+            dp[i + t] = max(dp[i + t], max(dp[:i + 1]) + p)
+
+    return max(dp)
+            
+if __name__ == "__main__":
+    N = int(input())
+    consulution_list = [list(map(int, input().split())) for _ in range(N)]
+
+    print(best_schedule(consulution_list))
