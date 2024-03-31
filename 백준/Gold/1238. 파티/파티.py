96f73712d01b4ec95affc3da16117645eaa88c45
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-03-31 15:50:15 +0900

    [Gold III] Title: 파티, Time: 248 ms, Memory: 114412 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,29 +1,46 @@
-import heapq
+# 파티
 
-def dijkstra(s):
-    D = [float('inf')] * (N+1)
-    D[s] = 0
-    q = []								    # 최단 거리 테이블을 heap으로 구현
-    heapq.heappush(q, (0, s))				# heap에 (가중치, 노드) 형식으로 삽입 
+# 다익스트라 알고리즘... 뭔지는 알고 있었지만 문제유형으로는 처음 봐서 전혀 감을 잡지 못함.
+# 그저 그래프 문제로 생각하고 dfs일지 bfs일지 고민하다가 bfs로 붙잡고 한 세월을 보냄.
+# bfs로도 분명 풀릴 것은 같은데 아마 시간초과가 나지 않을까 싶음.
+
+# +++ 늦어서 대충 보고 다시 문제를 이해하려고 살펴보니 그냥 다익스트라가 필요한 문제라는 것을 알았음.
+# 단방향 엣지, a에서 b로 가는 최단 경로를 구할 때의 경우에 다익스트라를 기억하자. 벨만 포드 알고리즘도 알아야 할 듯함.
+# 또 참고한 답안은 모든 노드에서 X로 가는 최단 거리를 구할 때, 다익스트라를 N - 1번 쓰지 않고, 시간을 줄이기 위해
+# 역방향 엣지 그래프를 새로 만들어서 X에서 다른 모든 노드로 가는 최단 거리를 구하는 문제로 바꾸었음.
+
+import sys, heapq
+input = sys.stdin.readline
+INF = 1e10
+
+def dijkstra(start, edge):
+    distance = [INF] * (N + 1)
+    q = []
+    heapq.heappush(q, (start, 0))
+
     while q:
-        dist, now = heapq.heappop(q)		# 최소힙이므로 가중치가 가장 작은 값이 pop
-        if D[now] >= dist:					# 이미 최솟값 구했는지 확인
-            for v, val in city[now]:		# 연결된 노드들 확인
-                if dist + val < D[v]:		# 가중치가 더 작은 값이면 갱신
-                    D[v] = dist + val
-                    heapq.heappush(q, (dist + val, v))
-    return D
+        ci, cd = heapq.heappop(q)
 
-N, M, X = map(int, input().split())
-city = [[] for _ in range(N+1)]
-for _ in range(M):
-    a, b, t = map(int, input().split())
-    city[a].append([b, t])
-ans = dijkstra(X)
-ans[0] = 0
-for i in range(1, N+1):
-    if i != X:
-        res = dijkstra(i)
-        ans[i] += res[X]
+        if distance[ci] >= cd:
+            for ni, nd in edge[ci]:
+                if distance[ni] > cd + nd:
+                    distance[ni] = cd + nd
+                    heapq.heappush(q, (ni, cd + nd))
 
-print(max(ans))
+    return distance
+
+
+if __name__ == "__main__":
+    N, M, X = map(int, input().split())
+    graph = [[] for _ in range(N + 1)]
+    reverse_graph = [[] for _ in range(N + 1)]
+
+    for _ in range(M):
+        a, b, c = map(int, input().split())
+        graph[a].append((b, c))
+        reverse_graph[b].append((a, c))
+    
+    go_to = dijkstra(X, reverse_graph)
+    come_back = dijkstra(X, graph)
+
+    print(max([go_to[i] + come_back[i] for i in range(1, N + 1) if i != X]))
