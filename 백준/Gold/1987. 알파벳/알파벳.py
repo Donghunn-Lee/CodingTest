25e56367db7b37307d36aafd1f1dbcf6c10bb15d
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-04-05 19:10:10 +0900

    [Gold IV] Title: 알파벳, Time: 408 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -3,38 +3,45 @@
 # bfs를 쓰려다가 문득 조건에 따라 방향을 바꿔 움직이는 건 dfs라는 것을 떠올려 dfs를 사용.
 # 그리고 예제1처럼 같은 문자가 2개 이상 인접해 있는 경우, 한 쪽을 선택해 방문 표시를 하고 나면 다른 쪽은 못 가므로
 # 백트래킹까지 해줘야겠다고 생각함. 입력 수가 20*20으로 현저히 적은 것에서도 추측함.
-# 방문한 좌표가 아니라 문자가 기준이므로, 이동가능한 최대 칸 수를 저장할 리스트와 방문한 문자 리스트를 각각 생성.
 
-# +++ 리스트를 썼다가 시간초과. in을 쓸 때 시간복잡도가 리스트는 O(n)이지만 집합은 O(1)이므로 집합을 써야함.
-# 그동안 graph문제에서 어떤 건 집합, 어떤 건 리스트를 써서 무슨 차이인가 했는데, in의 시간복잡도 차이였음.
-
+# +++ 재귀 dfs로 풀긴 했으나, 7800ms. 재귀 방식 자체도 문제고 방문 체크도 문제인 것 같아서 검색후 다른 코드를 참고.
+# visited의 각 좌표에 방문한 문자들을 순서대로 넣어서, 그 길이로 ans를 초기화.
+# 그리고 visited에 저장된 문자를 비교해서 방문 여부를 확인.
+# bfs에선 popleft를 쓰지만, dfs
 import sys
 input = sys.stdin.readline
 
-def dfs(ci, cj, count):
-    global ans
-    # 함수 실행 시 visited_alpha에 문자 추가.
-    visited_alpha.add(graph[ci][cj])
-    # 거리 최댓값 갱신
-    ans = max(ans, count)
+def dfs(si, sj, alpha):
+    stack = [(si, sj, alpha)]
+    visited = [[''] * C for _ in range(R)]
+    dir = ((1, 0), (0, 1), (-1, 0), (0, -1))
+    ans = 0
 
-    for d in dir:
-        ni, nj = ci + d[0], cj + d[1]
-        if 0 <= ni < R and 0 <= nj < C and graph[ni][nj] not in visited_alpha:
-            # 인덱스 내에서 이동하며, count + 1.
-            dfs(ni, nj, count + 1)
+    visited[si][sj] = alpha
     
-    # 함수에서 입력받은 좌표에서 갈 수 있는 방향을 모두 확인했다면 백트래킹을 위해 제거.
-    visited_alpha.remove(graph[ci][cj])
+    while stack:
+        ci, cj, alpha = stack.pop()
+
+        if ans < len(alpha):
+            ans = len(alpha)
+        
+        if ans == 26:
+            return ans
+
+        for d in dir:
+            ni, nj = ci + d[0], cj + d[1]
+
+            if 0 <= ni < R and 0 <= nj < C and graph[ni][nj] not in alpha:
+                nxt_alpha = alpha + graph[ni][nj]
+
+                if visited[ni][nj] != nxt_alpha:
+                    visited[ni][nj] = nxt_alpha
+                    stack.append((ni, nj, nxt_alpha))
     
+    return ans
 
 if __name__ == "__main__":
     R, C = map(int, input().split())
     graph = [list(input().rstrip()) for _ in range(R)]
-    visited_alpha = set()
-    dir = ((1, 0), (0, 1), (-1, 0), (0, -1))
-    ans = 0
 
-    dfs(0, 0, 1)
-
-    print(ans)
+    print(dfs(0, 0, graph[0][0]))
