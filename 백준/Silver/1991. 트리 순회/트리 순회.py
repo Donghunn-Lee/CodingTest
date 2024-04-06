bb3db4c70b6a0fffda0b3168dc9d95148cead938
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-04-06 14:22:44 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -4,42 +4,29 @@
 # 그래서 그냥 패턴이 금방 보일 것 같은 재귀로 풀이.
 # 전위 순환은 함수 실행시 문자 추가, 중위는 반복 내에서 추가, 후위는 반복 후에 추가 하면 해결.
 
+#+++ 처음에 각각 함수를 만들었다가, 트리의 탐색 순서가 동일하다는 것을 깨닫고 하나의 함수에 세 가지 경우를 동시에 계산.
+
 import sys
 input = sys.stdin.readline
 
-def preorder(parent):
-    global ans1
-    ans1 += parent
+def cycles(parent):
+    global preorder, inorder, postorder
+    preorder += parent
 
     for i in tree[parent]:
         if i != '.':
-            preorder(i)
+            cycles(i)
 
-def inorder(parent):
-    global ans2
+        if parent not in inorder:
+            inorder += parent
 
-    for i in tree[parent]:
-        if i != '.':
-            inorder(i)
-    
-        if parent not in ans2:
-            ans2 += parent
-        
+    postorder += parent
 
-def postorder(parent):
-    global ans3
-
-    for i in tree[parent]:
-        if i != '.':
-            postorder(i)
-    
-    ans3 += parent
-        
 
 if __name__ == "__main__":
     N = int(input())
     tree = dict()
-    ans1, ans2, ans3 = '', '', ''
+    preorder, inorder, postorder = '', '', ''
 
     for i in range(N):
         abc = input().rstrip().split()
@@ -49,10 +36,8 @@ if __name__ == "__main__":
 
         tree[abc[0]] = abc[1:]
 
-    preorder(root)
-    inorder(root)
-    postorder(root)
+    cycles(root)
 
-    print(ans1)
-    print(ans2)
-    print(ans3)
+    print(preorder)
+    print(inorder)
+    print(postorder)
