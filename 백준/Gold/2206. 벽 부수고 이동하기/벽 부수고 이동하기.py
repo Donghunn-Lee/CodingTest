70007158c834340498a2cfe70c5f72e6dd083fcd
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-04-08 12:26:57 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 988 ms, Memory: 228128 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -17,15 +17,15 @@ def bfs(i, j, k):
     deq.append((i, j, k))
 
     # 벽 돌파 여부를 모두 체크하는 3차원 리스트로 생성.
-    visited = [[[False] * 2 for _ in range(M)] for _ in range(N)]
+    visited = [[[False] * M for _ in range(N)] for _ in range(2)]
     visited[i][j][k] = 1    # 시작 지점 초기화.
 
     while deq:
-        ci, cj, broke = deq.popleft()
+        broke, ci, cj = deq.popleft()
 
         # 목적지에 도달한 경우 해당 값을 리턴.
         if (ci, cj) == (N - 1, M - 1):
-            return visited[ci][cj][broke]
+            return visited[broke][ci][cj]
 
         for di, dj in ((1, 0), (0, 1), (-1, 0), (0, -1)):
             ni, nj = ci + di, cj + dj
@@ -34,17 +34,17 @@ def bfs(i, j, k):
             if 0 <= ni < N and 0 <= nj < M:
                 
                 # 다음 칸이 갈 수 있는 길이고 아직 방문하지 않은 경우 deq에 추가.
-                if graph[ni][nj] == '0' and not visited[ni][nj][broke]:
-                    visited[ni][nj][broke] = visited[ci][cj][broke] + 1
-                    deq.append((ni, nj, broke))
+                if graph[ni][nj] == '0' and not visited[broke][ni][nj]:
+                    visited[broke][ni][nj] = visited[broke][ci][cj] + 1
+                    deq.append((broke, ni, nj))
 
                 # 다음 칸이 벽이며 아직 벽을 하나도 뚫지 않은 경우,
-                # 벽을 뚫은 경로를 저정하는 visited[][][1]에 현재 경로의 값 + 1을 할당.
+                # 벽을 뚫은 경로를 저정하는 visited에 현재 경로의 값 + 1을 할당.
                 # 이후 덱에도 기존 0이었던 broke가 아닌 1로 바꾸어 추가.
-                # 이 때 추가된 튜플을 기점으로 visited[][][1]의 맵이 갱신되기 시작하여 [0]와 병행하여 탐색.
+                # 이 때 추가된 튜플을 기점으로 visited[1][][]의 맵이 갱신되기 시작하여 [0]와 병행하여 탐색.
                 elif graph[ni][nj] == '1' and not broke:
-                    visited[ni][nj][1] = visited[ci][cj][broke] + 1
-                    deq.append((ni, nj, 1))
+                    visited[1][ni][nj] = visited[broke][ci][cj] + 1
+                    deq.append((1, ni, nj))
 
     # 목적지에 도달하지 못하고 반복이 종료될 경우 -1을 리턴.
     return -1
@@ -52,6 +52,6 @@ def bfs(i, j, k):
 
 if __name__ == "__main__":
     N, M = map(int, input().split())
-    graph = [list(input().rstrip()) for _ in range(N)]
+    graph = [input().rstrip() for _ in range(N)]
 
     print(bfs(0, 0, 0))
