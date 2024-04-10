05bd377277cc66100ab008a2b0fef7a47cbb58ee
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-04-10 10:26:49 +0900

    [Silver III] Title: 파도반 수열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,23 @@
+# 파도반 수열
+
+import sys
+input = sys.stdin.readline
+
+def fractal_triangle(n):
+    seq = [0, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9]
+
+    for i in range(11, n + 1):
+        seq.append(seq[i - 1] + seq[i- 5])
+
+    return str(seq[n])
+
+
+if __name__ == "__main__":
+    T = int(input())
+    ans = []
+
+    for _ in range(T):
+        N = int(input())
+        ans.append(fractal_triangle(N))
+    
+    print('\n'.join(ans))
