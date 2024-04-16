88b19c707cfe2811e56bf25a258204510451c201
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-04-16 11:37:15 +0900

    [Gold IV] Title: 문자열 폭발, Time: 204 ms, Memory: 58008 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -3,6 +3,7 @@
 # 스텍 문제인가? 아님 그냥 구현 문제인가 유형은 정확히 모르겠음.
 # 다만 이런 문제가 유형 파악이 쉬운 여타 알고리즘 문제들보다 의외로 머리를 더 많이 쓰게 하는 것 같음.
 # 다행히도 이 문제는 50분 안쪽으로 풀긴 했으나, 조금만 복잡해져도 소요시간은 두 배로 뛸듯함.
+#ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -11,6 +12,7 @@ input = sys.stdin.readline
 def removal_bomb(string, bomb):
     bomb_length = len(bomb)
     string_length = len(string)
+    flag = bomb[-1]
     result = []
 
     # 입력 문자열보다 폭탄 길이가 더 크면 폭탄이 존재 할 수 없으므로 그대로 반환.
@@ -21,8 +23,9 @@ def removal_bomb(string, bomb):
     for s in string:
         result.append(s)
 
-        # result의 뒤에서 폭탄 길이만큼의 문자가 폭탄과 일치할 경우, 뒤에서부터 폭탄 수만큼 제거..
-        if result[-bomb_length :] == bomb:
+        # result의 뒤에서 폭탄 길이만큼의 문자가 폭탄과 일치할 경우, 뒤에서부터 폭탄 수만큼 제거.
+        # +++ 폭탄의 마지막 문자를 flag에 저장해두고, result의 마지막 문자가 flag일 경우에만 조건 체크.
+        if s == flag and result[- bomb_length :] == bomb:
             del result[-bomb_length:]
     
     # 폭탄 제거 후 result에 문자가 남아있는지 여부에 따라 출력문을 반환.
