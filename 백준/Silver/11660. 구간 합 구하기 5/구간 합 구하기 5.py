0d34184f6f6d413579ca7dcc9ab17722aac59792
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-04-19 11:32:34 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 824 ms, Memory: 117760 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -3,7 +3,6 @@
 # 당연히 안 될 걸 알고 그냥 sum으로 한 번 풀어봤다가 시간초과. 그저 시간 복잡도 계산을 해 보고 싶었음.
 # 분류를 확인하니 그럴 것 같긴 했지만 dp이길래, 어떻게 활용할 수 있을까 고민하다가 해답을 떠올려냄.
 # dp 테이블에 (0, 0)에서부터 어떤 좌표까지의 합을 계산하여 저장하면 약간의 계산으로 풀 수 있었음.
-
 import sys
 input = sys.stdin.readline
 
@@ -52,6 +51,5 @@ if __name__ == "__main__":
                 result += dp[x1 - 1][y1 - 1]
 
         ans.append(result)
-            
-    
+              
     print("\n".join(map(str, ans)))
