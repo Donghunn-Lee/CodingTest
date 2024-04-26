6d840581ccb767a55bdb11c9b237771ec0e7593f
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-04-26 17:57:38 +0900

    [Gold IV] Title: 연구소, Time: 1576 ms, Memory: 34148 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,9 +1,14 @@
 # 연구소
 
+# 벽을 세울 수 있는 모든 조합을 찾고, 그 다음 바이러스의 위치에서 탐색을 통해 안전한 공간의 수를 세야 하는 문제.
+# 아마 모듈로 조합을 뽑을 수도 있을텐데, dfs로 해도 시간 초과가 안 나는지 확인하기 위해 dfs를 사용. 통과됨.
+# 백준에서 dfs와 bfs를 같이 쓰는 문제는 처음이지만(bfs 대신 dfs를 또 써도 되긴 할 텐데) 골드 4라 크게 어렵진 않았음.
+
 import sys
 from collections import deque
 input = sys.stdin.readline
 
+# 바이러스와 빈 공간의 위치 정보를 얻기 위한 함수.
 def location(graph):
     virus = []
     empty_space = []
@@ -17,7 +22,8 @@ def location(graph):
 
     return virus, empty_space
 
-def bfs(num_empty):
+# dfs에서 만든 조합으로 벽을 막은 상태에서 바이러스 전파를 시뮬레이션.
+def simulation(num_empty):
     global ans
 
     deq = deque(virus)
@@ -38,9 +44,10 @@ def bfs(num_empty):
     
     ans = max(ans, num_empty - infected_space - 3)
 
-def dfs(cur, count, limit):
+# 3개의 벽을 세울 수 있는 모든 조합을 찾고, 조합이 완성될 때마다 바이러스 전파 시뮬레이션을 돌리는 함수.
+def build_wall(cur, count, limit):
     if count == 3:
-        bfs(limit)
+        simulation(limit)
         return
     
     if cur == limit:
@@ -49,10 +56,10 @@ def dfs(cur, count, limit):
     i, j = empty_space[cur]
 
     laboratory[i][j] = 1
-    dfs(cur + 1, count + 1, limit)
+    build_wall(cur + 1, count + 1, limit)
     laboratory[i][j] = 0
 
-    dfs(cur + 1, count, limit)
+    build_wall(cur + 1, count, limit)
     
 
 
@@ -63,6 +70,6 @@ if __name__ == "__main__":
     virus, empty_space = location(laboratory)
     ans = 0
 
-    dfs(0, 0, len(empty_space))
+    build_wall(0, 0, len(empty_space))
 
     print(ans)
