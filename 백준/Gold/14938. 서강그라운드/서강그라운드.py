71f64c354ab0a4b0bea8acb29cffd861b1a15f2c
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-04-27 23:55:01 +0900

    [Gold IV] Title: 서강그라운드, Time: 128 ms, Memory: 110668 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.py
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -1,41 +1,30 @@
-import heapq
 import sys
 INF = int(1e9)
 n, m, r = map(int, input().split())
 
 items = [0] + [int(x) for x in sys.stdin.readline().split()]
 
-graph = [[] for _ in range(n + 1)]
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
 
 for _ in range(r):
     a, b, l = map(int, sys.stdin.readline().split())
-    graph[a].append([b, l])
-    graph[b].append([a, l])
+    graph[a][b] = l
+    graph[b][a] = l
 
-def dijkstra(start):
-    que = []
-    heapq.heappush(que, (0, start))
-    D[start] = 0
+for i in range(n + 1):
+    graph[i][i] = 0
 
-    while que:
-        dist, now = heapq.heappop(que)
-        if D[now] < dist:
-            continue
-
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < D[i[0]]:
-                D[i[0]] = cost
-                heapq.heappush(que, (cost, i[0]))
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
 ans = 0
 for i in range(1, n + 1):
-    D = [INF] * (n + 1)
-    dijkstra(i)
     tmp = 0
-    for d in range(1, n + 1):
-        if m >= D[d]:
-            tmp += items[d]
+    for j in range(1, n + 1):
+        if graph[i][j] <= m:
+            tmp += items[j]
 
     ans = max(ans, tmp)
 
