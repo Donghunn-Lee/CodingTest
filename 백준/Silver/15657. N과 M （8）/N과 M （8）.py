33a4f4c46ad2eb8df9e46b1f6886003cfd8781b9
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-05-05 18:34:28 +0900

    [Silver III] Title: N과 M (8), Time: 144 ms, Memory: 111740 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,23 @@
+# N과 M (8)
+
+import sys
+input = sys.stdin.readline
+
+def backtracking(count):
+    if count == M:
+        print(" ".join(map(str, ans)))
+        return 
+    
+    for i in seq:
+        if not ans or ans[-1] <= i:
+            ans.append(i)
+            backtracking(count + 1)
+            ans.pop()
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    seq = list(map(int, input().split()))
+    seq.sort()
+    ans = []
+
+    backtracking(0)
