bc540539dfefd4da445b721fb2552f0d68956a8e
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-05-05 17:37:47 +0900

    [Silver III] Title: N과 M (5), Time: 164 ms, Memory: 38556 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -11,20 +11,24 @@ def dfs(n, count):
     for i in range(len(seq)):
         if not tmp:
             tmp.append(seq[i])
+            tmp2.add(seq[i])
             dfs(n + 1, count + 1)
             tmp.pop()
+            tmp2.remove(seq[i])
 
-        elif seq[i] not in tmp:
+        elif seq[i] not in tmp2:
             tmp.append(seq[i])
+            tmp2.add(seq[i])
             dfs(n + 1, count + 1)
             tmp.pop()
+            tmp2.remove(seq[i])
         
 
 if __name__ == "__main__":
     N, M = map(int, input().split())
     seq = list(map(int, input().split()))
     seq.sort()
-    ans, tmp = [], []
+    ans, tmp, tmp2 = [], [], set()
 
     dfs(0, 0)
 
