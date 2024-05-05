9e5939dc09201a3a4fb778a501089cf2dff7f527
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-05-05 18:08:40 +0900

    [Silver II] Title: N과 M (9), Time: 184 ms, Memory: 118320 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,41 @@
+# N과 M (5)
+
+import sys
+input = sys.stdin.readline
+
+def dfs(n, count):
+    if count == M:
+        result.add(tuple(tmp))
+        return
+    
+    for i in range(len(seq)):
+        if not tmp:
+            tmp.append(seq[i])
+            tmp2.add(i)
+            dfs(n + 1, count + 1)
+            tmp.pop()
+            tmp2.remove(i)
+
+        elif i not in tmp2:
+            tmp.append(seq[i])
+            tmp2.add(i)
+            dfs(n + 1, count + 1)
+            tmp.pop()
+            tmp2.remove(i)
+        
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    seq = list(map(int, input().split()))
+    seq.sort()
+    result, tmp, tmp2 = set(), [], set()
+    ans = []
+
+    dfs(0, 0)
+
+    result = sorted(list(result))
+
+    for i in result:
+        ans.append(' '.join(map(str, i)))
+
+    print("\n".join(map(str, ans)))
