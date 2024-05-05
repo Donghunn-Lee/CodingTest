494e65165a10ebafe0ad3ea90c2ac0ceac73c50e
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-05-05 18:45:35 +0900

    [Silver II] Title: N과 M (12), Time: 128 ms, Memory: 110856 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,29 @@
+# N과 M (12)
+
+import sys
+input = sys.stdin.readline
+
+def backtracking(count):
+    if count == M:
+        print(" ".join(map(str, ans)))
+        return 
+    
+    last = -1
+
+    for i in range(N):
+        if last == seq[i]:
+            continue
+
+        if not ans or ans[-1] <= seq[i]:
+            ans.append(seq[i])
+            backtracking(count + 1)
+            last = ans.pop()
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    seq = list(map(int, input().split()))
+    seq.sort()
+    ans = []
+
+    backtracking(0)
+    
