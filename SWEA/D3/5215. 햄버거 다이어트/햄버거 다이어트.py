db7cbf275985dc8b5190707923bd73f0d16166d5
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-05-06 21:03:09 +0900

    [D3] Title: 햄버거 다이어트, Time: 124 ms, Memory: 50,032 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,41 +1,28 @@
 # 햄버거 다이어트
 
-# dfs는 당연히 시간초과 날 것 같아서 정렬이나 dp를 잘 사용해야겠다고 생각함.
-# 그런데 dp를 어떻게 해야할 지 몰라서 한참 고민.
-# 최근 풀었던 '가능한 시험 점수' 문제처럼 0에서부터 score를 더해가며 결과값을 리스트에 저장,
-# 그 결과값 리스트의 모든 요소에 현재의 score를 모두 더하고 그 결과를 다시 저장하는 방식이면 풀릴 거라고 판단.
-# 리스트 대신 딕셔너리를 사용. 그 딕셔너리를 for문으로 돌리고 내부에서 요소를 추가하니
-# dictionary changed size during iteration 에러 발생.
-# 시험 점수 문제는 시험 점수라는 하나의 도메인만 계산하지만 이 문제는 kcal가 있어서 length로 반복을 하기 어려웠음.
-# 그래서 생각한 건 결과값을 바로 추가하지 않고 임시 딕셔너리를 만들어 결과값의 for문이 끝난 뒤 추가하는 방식으로 진행.
-# 제출 결과는 pass. 하지만 더 간단한 방법이 분명 있을 거라고 생각함. 
+# dfs로 안 풀리는 줄 알고 한참 고민해서 기어코 dp로 풀어서 pass에 성공.
+# 그런데 dfs로 더 빠르게 풀리는 문제였음. 상당히 허탈함.
+# 풀던 도중에 dfs로 정말 안 될까? 싶어서 대충 몇 분만에 짜서 돌려봤는데 maxRecursion이 발생.
+# swea는 import sys를 못 쓰니 재귀 호출 제한을 못 늘려서 안 되는 줄 알았음.
+# 내가 안 됐던 이유는 dfs로 넘기는 건 어려울 게 없었는데 종료 조건을 sum_kcal > L 이거 하나만 설정했기 때문.
+# i가 N에 도달한 경우에 종료가 2번째,
+# 지금까지 더한 점수에 아직 확인 안 한 점수를 모두 더해도 지금까지 갱신된 max_score보다 작은 경우가 3번째였음.
 
-# ㅋㅋㅋ dfs로 풀리는 문제였음. 고민하다가 dfs로 정말 안 될까? 싶어서 대충 몇 분만에 짜서 돌려봤는데 maxRecursion이 뜨기에 swea는 import sys를 못 쓰니 안될 줄 알았는데 됨. 내가 종료 조건을 kcal > L 하나만 설정해서 그런 거였음.
+def dfs(i, sum_score, sum_kcal):
+    global max_score
 
-def best_burger():
-    dp = {0 : 0}
-
-    for score, kcal in score_and_kcal:
-        tmp = dict()
-
-        for l_k in dp:
-            n_s, n_k = dp[l_k] + score, l_k + kcal
-
-            if n_k > L:
-                continue
-            
-            if tmp.get(n_k):
-                tmp[n_k] = max(tmp[n_k], n_s)
-            else:
-                tmp[n_k] = n_s
-        
-        for t in tmp:
-            if dp.get(t):
-                dp[t] = max(dp[t], tmp[t])
-            else:
-                dp[t] = tmp[t]
+    if i == N:
+        max_score = max(max_score, sum_score)
+        return
     
-    return max(dp.values())
+    if sum_score + sum(score[i:]) <= max_score:
+        return
+    
+    if sum_kcal + kcal[i] <= L:
+        dfs(i + 1, sum_score + score[i], sum_kcal + kcal[i])
+
+    dfs(i + 1, sum_score, sum_kcal)
+
 
 if __name__ == "__main__":
     T = int(input())
@@ -43,8 +30,17 @@ if __name__ == "__main__":
 
     for t in range(1, T + 1):
         N, L = map(int, input().split())
-        score_and_kcal = [list(map(int, input().split())) for _ in range(N)]
+        score, kcal = [], []
+        
+        for _ in range(N):
+            a, b = map(int, input().split())
+            score.append(a)
+            kcal.append(b)
 
-        ans.append(f"#{t} {best_burger()}")
+        max_score = 0
+
+        dfs(0, 0, 0)
+
+        ans.append(f"#{t} {max_score}")
 
     print("\n".join(ans))
