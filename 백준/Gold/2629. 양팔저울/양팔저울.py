1bbb5225758a65f3899219e1ca20539bc8dd1ec4
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-05-08 18:35:38 +0900

    [Gold III] Title: 양팔저울, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -0,0 +1,61 @@
+# 양팔저울
+
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+def weighing(weights, targets):
+    cases = [0]
+    visited = [0] * (sum(weights) + 1)
+    result = {i : 'N' for i in targets}
+
+    for w in weights:
+        
+        for i in range(len(cases)):
+            tmp = [cases[i] + w]
+            tmp.append(abs(w - cases[i]))
+        
+            for t in tmp:
+                if visited[t] == 0:
+                    visited[t] = 1
+                    cases.append(t)
+                else:
+                    continue
+
+                if t in targets:
+                    result[t] = 'Y'
+    
+    return result
+            
+
+if __name__ == "__main__":
+    N = int(input())
+    weights = list(map(int, input().split()))
+    M = int(input())
+    target_list = list(map(int, input().split()))
+    ans = []
+
+    result = weighing(weights, set(target_list))
+
+    for t in target_list:
+        ans.append(result[t])
+    
+    print(" ".join(ans))
+    
+    
+    
+    
+    
+    
+    
+    
+    #ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+    #ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+    #ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ#ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ#ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ#ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ#ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ#ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ#ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ#ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ#ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ#ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ#ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+    
+    
+    
+    
+    
+    
+    
