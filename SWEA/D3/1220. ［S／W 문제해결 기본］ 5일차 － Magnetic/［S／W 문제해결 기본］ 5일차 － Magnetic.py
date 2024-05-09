553312a1200103e2c59b39efbc965f6a7743cbde
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-05-09 16:56:19 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 200 ms, Memory: 49,952 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -9,24 +9,33 @@
 def magnetic_field_activate():
     deadlock = 0
     
+    # 전체 그래프를 1회 탐색.
     for i in range(N):
         for j in range(N):
+            # N극 자성체를 발견한 경우.
             if graph[i][j] == 1:
-                cur = i + 1
+                cur = i + 1     # 비교를 위해 + 1한 위치에서 시작.
 
                 while True:
+                    # 자성체가 떨어지거나 같은 극 자성체를 만난 경우,
+                    # 붙어버린 같은 극의 자성체는 1개로 간주하므로 이동중인 자성체의 원래 위치에 0을 할당해 없애버림.
                     if cur == N or graph[cur][j] == 1:
                         graph[i][j] = 0
                         break
 
+                    # 빈 공간이면 계속해서 이동.
                     if graph[cur][j] == 0:
                         cur += 1
 
+                    # 반대 극의 자성체를 만난 경우, 이 둘은 교착 상태이며 사이에 다른 자성체가 없음을 확인.
+                    # 여기서 두 자성체를 다른 값으로 바꿔 고정시키는 방법 등도 고려해봤으나,
+                    # 그렇게 하면 뒤에서 같은 극의 자성체가 붙게 되는 경우 곤란해짐. 그래서 그냥 deadlock + 1.
                     elif graph[cur][j] == 2:
                         deadlock += 1
                         break
 
-            if graph[i][j] == 2:
+            # S극 자성체를 발견한 경우. 위와 방향만 바꾸어 동일하게 진행.
+            elif graph[i][j] == 2:
                 cur = i - 1
 
                 while True:
@@ -41,6 +50,7 @@ def magnetic_field_activate():
                         deadlock += 1
                         break
     
+    # 1쌍의 교착상태를 이루는 두 자성체가 각각 deadlock을 증가시키므로 2로 나누어 반환.
     return deadlock // 2
 
 if __name__ == "__main__":
@@ -53,4 +63,3 @@ if __name__ == "__main__":
         ans.append(f'#{t} {magnetic_field_activate()}')
     
     print("\n".join(ans))
-    
