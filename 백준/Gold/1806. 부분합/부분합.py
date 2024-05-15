27b85930434f9cf152b9c13d62cf4fe3ac1aa12b
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-05-15 23:48:29 +0900

    [Gold IV] Title: 부분합, Time: 136 ms, Memory: 120932 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,26 +1,48 @@
+# 부분합
+
+# 수열에서 조건을 만족하는 부분합을 구하고, 그 부분합 중 최소 길이의 부분합을 찾아 길이를 리턴하는 문제.
+# 골드 12만 풀다 4를 만나니 확실히 방법까지 도달할 수 있는 게 뭔가 맘이 편했음.
+# 투 포인터를 써야겠다고 바로 접근하여 푸는데, 문제는 합을 구하는 방식.
+# 나는 sum(seq[start:end])를 사용함. 저번 문제에서 사용했던 기억이 있었기 때문.
+# 하지만 아니었음. sum을 구하는 것도 길이만큼 시간복잡도가 들어감. 답은 포인터를 움직일 때 해당 값을 바로 더하고 빼기.
+# 그것만 생각하면 꽤 쉬운 문제.
+
 import sys
+input = sys.stdin.readline
+INF = 1e9
 
-N, S = map(int, input().split())
-numbers = list(map(int, input().split()))
+def two_pointer(seq):
+    start, end = 0, 0
+    min_length = INF
+    tmp_sum = 0
 
-left, right = 0, 0 # 두 개의 포인터는 0에서 부터 시작
-sum = 0 # 합을 저장할 변수
-min_length = sys.maxsize # 먼저 최대 길이로 지정
+    while True:
+        
+        # 이 조건을 제대로 이해하지 못해서 다른 코드로도 풀려야 하는 게 아닌가? 하고 한참 고민함.
+        # 나는 그냥 end가 최대 길이에 도달하면 바로 종료시켜버렸는데,
+        # end를 최대 길이에 세워두고 start를 좁혀도 여전히 S보다 tmp_sum이 큰 경우가 존재함. 당연한 건데 깨닫지 못함.
+        if S <= tmp_sum:
+            min_length = min(min_length, end - start)
+            tmp_sum -= seq[start]
+            start += 1
 
-while True:
-    # 만약 총 합이 S가 넘는다면, left를 하나씩 옮겨보면서 어디까지 길이가 줄어드나 확인
-    if sum >= S:
-        min_length = min(min_length, right - left)
-        sum -= numbers[left]
-        left += 1
-    elif right == N:
-        break
-    # 만약 총합이 S를 넘지 않는다면, right 을 오른쪽으로 한칸씩 옮기며 총합이 S를 넘을때까지 더함
+        # end가 N이면 반복을 종료하지만, S <= tmp_sum 이 False가 될 때 이 elif 문이 실행되므로 길이를 좁힐 수 있음.
+        elif end == N:
+            break
+    
+        else:
+            tmp_sum += seq[end]
+            end += 1
+
+        
+    if min_length == INF:
+        return 0
+    
     else:
-        sum += numbers[right]
-        right += 1
+        return min_length
 
-if min_length == sys.maxsize:
-    print(0)
-else:
-    print(min_length)
+if __name__ == "__main__":
+    N, S = map(int, input().split())
+    seq = list(map(int, input().split()))
+
+    print(two_pointer(seq))
