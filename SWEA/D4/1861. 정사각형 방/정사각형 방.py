86a3c4efcabcb9b64786cf01da6a137627a382eb
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-05-16 20:53:22 +0900

    [D4] Title: 정사각형 방, Time: 1,277 ms, Memory: 54,808 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.py
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -1,12 +1,17 @@
 # 정사각형 방
 
+# 전형적인 bfs/dfs 문제. 뭐가 더 효율적인지는 잘 모르겠음.
+# 시작하는 방부터 1로 치는 걸 몰라서 5분 더 걸림.
+# 매 방마다 딱 1씩 더 큰 방으로 이동할 수 있기 때문에 visited가 필요 없었음.
+# 모든 좌표에서 bfs를 돌리고, 이동 횟수를 리턴. 최대치를 저장해가며 같은 경우 더 작은 방 번호로 갱신함.
+
 from collections import deque
 
 def bfs(si, sj):
     deq = deque()
     deq.append((si, sj, 1))
     moved = 0
-
+    
     while deq:
         ci, cj, cnt = deq.popleft()
 
