5f73ed973f6c7bb4d5ffa9fd2d45c155d0978c1a
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-05-22 18:27:35 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 4424 ms, Memory: 149652 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
@@ -1,28 +1,39 @@
+# Dance Dance Revolution
+
+# dfs로 호기롭게 풀다가 시간초과.
+# n이 
 import sys
-sys.setrecursionlimit(10**6)
- 
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+
 def move(a, b):
     if a == b:
         return 1
+    
     elif a == 0:
         return 2
-    elif abs(b-a)%2 == 0:
+    
+    elif abs(b - a) % 2 == 0:
         return 4
+    
     else:
         return 3
- 
+
+
+
 def solve(n, l, r):
-    global dp
-    if n >= len(arr)-1:
+    if n >= len(seq)-1:
         return 0
  
     if dp[n][l][r] != -1:
         return dp[n][l][r]
  
-    dp[n][l][r] = min(solve(n+1, arr[n],r) + move(l, arr[n]), solve(n+1, l, arr[n]) + move(r, arr[n]))
+    dp[n][l][r] = min(solve(n+1, seq[n],r) + move(l, seq[n]), solve(n+1, l, seq[n]) + move(r, seq[n]))
     return dp[n][l][r]
- 
-arr = list(map(int, sys.stdin.readline().split()))
-dp = [[[-1]*5 for _ in range(5)] for _ in range(100000)]
- 
-print(solve(0, 0, 0))
+
+
+if __name__ == "__main__":
+    seq = list(map(int, input().split()))
+    dp = [[[-1]*5 for _ in range(5)] for _ in range(100000)]
+    
+    print(solve(0, 0, 0))
