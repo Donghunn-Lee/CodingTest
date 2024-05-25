5948cee58f0e57ae2d6abb2f00d6bd60b2a7ab1d
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-05-25 17:08:41 +0900

    [Gold III] Title: 소가 길을 건너간 이유 6, Time: 348 ms, Memory: 35848 KB -BaekjoonHub

--- 백준/Gold/14466. 소가 길을 건너간 이유 6/소가 길을 건너간 이유 6.py
+++ 백준/Gold/14466. 소가 길을 건너간 이유 6/소가 길을 건너간 이유 6.py
@@ -1,55 +1,64 @@
-"""
-  정해진 길을 이용해야하는 소들 쌍
-"""
+# 소가 길을 건너간 이유 6
+
+# 길이 어떻게 되는건지 처음엔 이해가 잘 안돼서 좀 해멨던 문제.
+# 길은 칸으로 구분 하는 것이 아니라, 두 점의 연결로 묶어 생각해야했음. 그 부분을 제외하면 평범.
+
 import sys
 from collections import deque
+input = sys.stdin.readline
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+def bfs():
+    deq = deque()
+    count = 0   # 길을 건너야만 만날 수 있는 소 쌍의 수
 
-def bfs(r1, c1):
-    dq = deque()
-    dq.append((r1, c1))
-    cow_visit[r1][c1] = True
-    while dq:
-        x, y = dq.popleft()
-        for d in range(4):
-            nx = dx[d] + x
-            ny = dy[d] + y
-            # 범위 체크
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
-                continue
-            if cow_visit[nx][ny]:  # 방문 체크
-                continue
-            if (nx, ny) in road[x][y]:  # 다리인 경우 pass
-                continue
-            dq.append((nx, ny))
-            cow_visit[nx][ny] = True
+    # 소를 한 마리씩 이동시키며 탐색.
+    for i in range(len(cows)):
+        deq.append(cows[i])
+        visited = [[0] * (N + 1) for _ in range(N + 1)]
+        
+        # bfs반복
+        while deq:
+            ci, cj = deq.popleft()
 
-n, k, r = map(int, sys.stdin.readline().split())  # n*n, k: 마리, r: 정해진 길 
+            for di, dj in ((0, 1), (1, 0), (-1, 0), (0, -1)):
+                ni, nj = ci + di, cj + dj
 
-road = [[[] for _ in range(n)] for _ in range(n)]
-cow_visit = [[False] * n for _ in range(n)]
-count = 0
-# 길 위치 담기
-for _ in range(r):
-    r1, c1, r2, c2 = map(int, sys.stdin.readline().split())
-    road[r1 - 1][c1 - 1].append((r2 - 1, c2 - 1))
-    road[r2 - 1][c2 - 1].append((r1 - 1, c1 - 1))
+                if 0 < ni <= N and 0 < nj <= N and not visited[ni][nj]:
 
-# 소의 위치 담기
-cow_list = list()
-for _ in range(k):
-    a, b = map(int, sys.stdin.readline().split())
-    cow_list.append((a - 1, b - 1))
+                    # 현재 방문한 목초지가 길이 있는 점이며, 그 길이 이전 목초지에서 이어진 길인 경우,
+                    # 현재의 방문은 길을 건너온 셈이 되므로 continue.
+                    if (ni, nj) in road and (ci, cj) in road[(ni, nj)]:
+                        continue
 
-# 1. 소를 한마리씩 돌려가면서 방문 여부 탐색
-for i, cow in enumerate(cow_list):
-    cow_visit = [[False] * n for _ in range(n)]
-    # 2. 현재 소가 정해진 길 없이 가는 경로를 탐색
-    bfs(cow[0], cow[1])
-    for r2, c2 in cow_list[i + 1:]:
-        # 3. 방문을 완료하지 못한 경우 결과 + 1
-        if not cow_visit[r2][c2]:
-            count += 1
-print(count)
+                    deq.append((ni, nj))
+                    visited[ni][nj] = 1
+        
+        # 현재 소부터 아직 탐색하지 않은 소들 간의 접촉 여부를 체크.
+        for i, j in cows[i + 1:]:
+            if not visited[i][j]:
+                count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    N, K, R = map(int, input().split())
+    graph = [[0] * (N + 1) for _ in range(N + 1)]
+
+    road = dict()
+    for _ in range(R):
+        a, b, c, d = map(int, input().split())
+
+        if (a, b) in road:
+            road[(a, b)].add((c, d))
+        else:
+            road[(a, b)] = {(c, d)}
+
+        if (c, d) in road:
+            road[(c, d)].add((a, b))
+        else:
+            road[(c, d)] = {(a, b)}
+
+    cows = [tuple(map(int, input().split())) for _ in range(K)]
+
+    print(bfs())
