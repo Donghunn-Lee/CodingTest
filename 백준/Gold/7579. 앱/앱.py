4411eed3bfbd932e9b8c1a391b3a2f9233d8d378
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-05-29 13:05:32 +0900

    [Gold III] Title: 앱, Time: 140 ms, Memory: 114832 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -1,17 +1,19 @@
-n, m = map(int, input().split())
-memory = [0] + list(map(int, input().split()))
-cost = [0] + list(map(int, input().split()))
-length = sum(cost)+1
-dp = [[0 for _ in range(length)] for _ in range(n+1)]
-ans = 10001
+N, M = map(int, input().split())
+apps = list(map(int, input().split()))
+deact = list(map(int, input().split()))
 
-for i in range(1, n+1):
-    ci, mi = cost[i], memory[i]
-    for j in range(length):
-        dp[i][j] = dp[i-1][j]
-    for j in range(ci, length):
-        dp[i][j] = max(dp[i-1][j-ci] + mi, dp[i][j])
-        if dp[i][j] >= m:
-            ans = min(ans, j)
+# dp[비용][메모리]
+dp = [[0 for _ in range(10000+1)] for _ in range(N+1)]
+for i in range(1, N+1): # i 개의 앱 비활성화
+    for w in range(10000+1): # 사용 가능한 비활성화에 필요한 비용
+        if deact[i-1] <= w: # i-1 개의(리스트 시작 인덱스 = 1) 앱을 비활성화 시킬 수 있다면 
+        	# i개 비활성화로 확보된 메모리 = i-1 개 비활성화 + i-1 번 째 앱 메모리 (i-1 번째 비활성화)
+            # 						    i-1 번째 비활성화 X 중 최댓값
+            dp[i][w] = max(apps[i-1] + dp[i-1][w-deact[i-1]], dp[i-1][w])
+        else: # 비활성화 불가
+            dp[i][w] = dp[i-1][w]
 
-print(ans)
+for i, c in enumerate(dp[-1]): # 
+    if c >= M: # 처음으로 필요한 메모리보다 확보된 양이 많아진다면 출력
+        print(i)
+        break
