078b6ae9caf66b24f2f8a05a07bc5b0db83dc435
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-05-30 14:41:51 +0900

    [Gold IV] Title: LCS 2, Time: 596 ms, Memory: 476632 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,30 +1,38 @@
+# LCS 2
+
+# 꽤 전에 풀었던 LCS와 거의 동일하지만, 이번엔 길이만이 아니라 그 값도 구하는 문제.
+# 직전 문제가 냅색 알고리즘이었는데 이 문제도 2차원 dp테이블을 사용해서 조금 이해가 빨랐음.
+# 고비는 같은 문자를 찾았을 때, 어느 것을 기준으로 현재 dp를 갱신해야 하는지에 대한 부분. 여기서 좀 헤맸음.
+# 그 점만 이해하면 나머지는 쉬웠음. 길이 대신 문자열을 저장하는 것으로 문자열을 구해낼 수 있음.
+
 import sys
 input = sys.stdin.readline
 
-A = [""] + list(input().rstrip())
-B = [""] + list(input().rstrip())
-LCS = [[""]*len(B) for _ in range(len(A))]
+def LCS2(a, b):
+    dp = [[''] * len(b) for _ in range(len(a))]
 
-# LCS[i][j]는 A의 i번째까지의 문자열과
-# B의 j번째까지의 문자열의 LCS 길이 값을 의미함
-# LCS[i][j]에서, 만약 A[i] == B[j]라면
-# 그 두 문자를 제외한 i-1개, j-1개의 LCS
-# 만약 다르다면, i-1개, j개와 i개, j-1개
-# 일 때의 LCS 중 큰 값 취하기. i-1개, j-1개
-# 는 어차피 저 두 케이스보다 반드시 작거나 같으므로
-# 두 케이스만 고려.
-# 그런데 LCS 리스트 원소 값이 LCS 길이 값을 의미하므로,
-# 원소를 LCS 문자열 자체로 둬도 됨. 그럼 길이 값도 갖고
-# 최단 경로도 갖고 있게 됨.
-for i in range(1, len(A)):
-    for j in range(1, len(B)):
-        if A[i] == B[j]:
-            LCS[i][j] = LCS[i-1][j-1] + A[i]
-        else:
-            if len(LCS[i-1][j]) >= len(LCS[i][j-1]):
-                LCS[i][j] = LCS[i-1][j]
+    for i in range(1, len(a)):
+        for j in range(1, len(b)):
+            if a[i] == b[j]:
+                dp[i][j] = dp[i - 1][j - 1] + b[j]
+            
             else:
-                LCS[i][j] = LCS[i][j-1]
+                # 가능한 lcs중 아무거나 출력하면 되므로, 길이가 같은 경우엔 어느쪽이든 상관 없음. 나는 j - 1을 사용.
+                if len(dp[i][j - 1]) < len(dp[i - 1][j]):
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i][j - 1]
 
-result = LCS[-1][-1]
-print(len(result), result, sep="\n")
+    return dp[-1][-1]
+
+if __name__ == "__main__":
+    A = ' ' + input().rstrip()
+    B = ' ' + input().rstrip()
+
+    result = LCS2(A, B)
+    
+    print(len(result))
+    
+    if result:
+        print(result)
+    
