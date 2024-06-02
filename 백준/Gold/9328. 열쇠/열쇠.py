ec5067384583aeed81767e62b7c1f1125d6cda84
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-06-02 18:15:59 +0900

    [Gold I] Title: 열쇠, Time: 236 ms, Memory: 115060 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.py
+++ 백준/Gold/9328. 열쇠/열쇠.py
@@ -1,54 +1,70 @@
+# 열쇠
+
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-dr = [-1, 0, 1, 0]  # 상 우 하 좌
-dc = [0, 1, 0, -1]  # 상 우 하 좌
+def bfs():
+    global count
+
+    deq = deque()
+    deq.append((0, 0))
+    visited = [[0] * (W + 2) for _ in range(H + 2)]
+    visited[0][0] = 1
+
+    while deq:
+        ci, cj = deq.popleft()
+
+        for di, dj in ((0, 1), (1, 0), (-1, 0), (0, -1)):
+            ni, nj = ci + di, cj + dj
+
+            if 0 <= ni < H + 2 and 0 <= nj < W + 2 and not visited[ni][nj] and building[ni][nj] != '*':
+                nxt = building[ni][nj]
+
+                if nxt == '.':
+                    deq.append((ni, nj))
+                
+                elif nxt == '$':
+                    count += 1
+                    deq.append((ni, nj))
+                
+                elif 65 <= ord(nxt) <= 90:
+                    if chr(ord(nxt) + 32) in visited_keys:
+                        deq.append((ni, nj))
+
+                    else:
+                        if visited_door.get(nxt):
+                            visited_door[nxt].append((ni, nj))
+                        else:
+                            visited_door[nxt] = [(ni, nj)]
+
+                elif 97 <= ord(nxt) <= 122:
+                    visited_keys.add(nxt)
+                    deq.append((ni, nj))
+                    
+                    if visited_door.get(chr(ord(nxt) - 32)):
+                        for t_ni, t_nj in visited_door[chr(ord(nxt) - 32)]:                        
+                            deq.append((t_ni, t_nj))
+                    
+                visited[ni][nj] = 1
 
 
-def bfs(visited):
-    global ans
+if __name__ == "__main__":
+    T = int(input())
+    ans = []
 
-    queue = deque([[0, 0]])
-    visited[0][0] = True
-    while queue:
-        r, c = queue.popleft()
+    for _ in range(T):
+        H, W = map(int, input().split())
+        building = ['.' + input().rstrip() + '.' for _ in range(H)]
+        building = ['.' * (W + 2)] + building + ['.' * (W + 2)]
+        visited_door, visited_keys = dict(), set()
+        keys = list(input().rstrip())
+        for k in keys:
+            visited_keys.add(k)
 
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            # 범위를 벗어나거나 벽이거나 이미 방문했다면 컨티뉴
-            if nc < 0 or nc >= w + 2 or nr < 0 or nr >= h + 2 or miro[nr][nc] == '*' or visited[nr][nc]:
-                continue
+        count = 0
+        bfs()
 
-            if 'A' <= miro[nr][nc] <= 'Z':  # 대문자라면(문이라면)
-                if chr(ord(miro[nr][nc]) + 32) not in key:  # 해당 문을 열수있는 키가 없다면
-                    continue  # 컨티뉴
-            elif 'a' <= miro[nr][nc] <= 'z':  # 만약 소문자이고
-                if miro[nr][nc] not in key:  # 아직 키에 없다면
-                    key[miro[nr][nc]] = True  # 해당 키를 저장후
-                    visited = [[False] * (w + 2) for _ in range(h + 2)]  # 방문체크 초기화
-            elif miro[nr][nc] == "$" and (nr, nc) not in visited_doc:  # 비밀문서이고 아직방문하지 않았다면
-                ans += 1  # 찾은 개수 1개 증가
-                visited_doc.append((nr, nc))  # 해당 위치는 더이상 방문하면 안되기 때문에 저장
-
-            visited[nr][nc] = True  # 방문처리
-            queue.append([nr, nc])  # 다음 위치를 큐에 삽입
-
-
-T = int(input())
-
-for _ in range(1, T + 1):
-    h, w = map(int, input().split())
-
-    miro = ['.' + input() + '.' for _ in range(h)]
-    miro = ['.' * (w + 2)] + miro + ['.' * (w + 2)]
-    visited = [[False] * (w + 2) for _ in range(h + 2)]
-    key = {}  # 가지고 있는 키 저장
-    visited_doc = []  # 방문한 키 위치 저장
-
-    for i in input():
-        if i.isalpha():  # 만약 알파벳이면
-            key[i] = True  # 키로 저장
-
-    ans = 0
-    bfs(visited)
-    print(ans)
+        ans.append(str(count))
+    
+    print("\n".join(ans))
