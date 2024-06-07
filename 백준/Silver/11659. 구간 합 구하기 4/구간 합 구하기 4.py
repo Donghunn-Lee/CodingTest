e063d2f46edcfe1143dc913f5d962141608b021d
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-06-07 18:18:14 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 196 ms, Memory: 127532 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,15 +1,29 @@
+# 구간 합 구하기 4
+
+# 누적 합 문제.
+# 테스트 케이스가 여러개라서 당연히 한 번에 값을 다 구해두고, 주어진 인덱스를 뽑아 사용할 것을 생각함.
+# 그런데 인덱스를 어떻게 해야 할 지를 모르겠어서 찾아봄. 생각보다 더 단순했음.
+# [뒷 인덱스] - [앞 인덱스 - 1] 하면 앞에서 정해진 구간 앞에서 더한 값을 빼는 꼴이 됌.
+
 import sys
 input = sys.stdin.readline
- 
-m, n = map(int, input().split())
-arr = list(map(int, input().split()))
-prefix_sum = [0]    # init prefix_sum    
- 
-temp = 0    
-for i in arr:    # accumulate arr section 
-    temp += i
-    prefix_sum.append(temp)
- 
-for i in range(n):
-    a, b = map(int, input().split())
-    print(prefix_sum[b] - prefix_sum[a-1])
+
+def cal_prefix_sum():
+    tmp = 0
+
+    for i in seq:
+        tmp += i
+        prefix_sum.append(tmp)
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    seq = list(map(int, input().split()))
+    prefix_sum = [0]
+    cal_prefix_sum()
+    ans = []
+
+    for _ in range(M):
+        a, b = map(int, input().split())
+        ans.append(str(prefix_sum[b] - prefix_sum[a - 1]))
+    
+    print("\n".join(ans))
