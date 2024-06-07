59c9d4a8189a46602539bd553d34fc12a1c199c7
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-06-07 22:47:43 +0900

    [Silver II] Title: 좌표 압축, Time: 900 ms, Memory: 282104 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,19 @@
+# 좌표 압축
+
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    N = int(input())
+    seq = list(map(int, input().split()))
+    sorted_seq = sorted(set(seq))
+
+    compressioned = dict()
+
+    for i in range(len(sorted_seq)):
+        compressioned[sorted_seq[i]] = i
+    
+    for i in range(N):
+        seq[i] = compressioned[seq[i]]
+    
+    print(" ".join(map(str, seq)))
