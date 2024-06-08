2bf149fbbd4a61cdb2d4d522aa90d0e51c378c55
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-06-08 20:03:56 +0900

    [Silver IV] Title: 동전 0, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,22 @@
+# 동전 0
+
+import sys
+input = sys.stdin.readline
+
+def min_coins(k):
+    count = 0
+
+    for i in range(N - 1, -1, -1):
+        if coins[i] <= k:
+            tmp = k // coins[i]
+            count += tmp
+            k -= coins[i] * tmp
+        
+        if k == 0:
+            return count
+
+if __name__ == "__main__":
+    N, K = map(int, input().split())
+    coins = [int(input()) for _ in range(N)]
+
+    print(min_coins(K))
