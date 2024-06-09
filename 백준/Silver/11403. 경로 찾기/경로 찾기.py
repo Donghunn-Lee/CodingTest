2f8abf9931fb1da620fd85a5a7e9c9d624043efa
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-06-09 15:13:42 +0900

    [Silver I] Title: 경로 찾기, Time: 144 ms, Memory: 113980 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,16 +1,18 @@
 # 경로 찾기
 
+# 직관적으로 이해는 되는데 값을 갱신하는 과정이 조금 햇갈렸던 문제.
+# dfs나 bfs 모두 사용 가능했음. 나는 처음부터 dfs를 생각했기에 dfs를 사용.
+# 풀긴 했는데 시간이 너무 길어서, 제대로 된 방법을 검색.
+# visited를 그냥 1차원으로 해서 현재 탐색의 시작노드만 가지고 값을 갱신하는 방법이 있었음.
+
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 5)
 
-def dfs(si, ci):
+def dfs(i):
     for j in range(N):
-        if graph[ci][j] == 1 and not visited[ci][j]:
-            graph[si][j] = 1
-            visited[ci][j] = 1
-            dfs(si, j)
-        
+        if graph[i][j] == 1 and not visited[j]:
+            visited[j] = 1
+            dfs(j)
 
 if __name__ == "__main__":
     N = int(input())
@@ -18,10 +20,8 @@ if __name__ == "__main__":
     ans = []
 
     for i in range(N):
-        visited = [[0] * N for _ in range(N)]
-        dfs(i, i)
-    
-    for i in range(N):
-        ans.append(" ".join(map(str, graph[i])))
+        visited = [0 for _ in range(N)]
+        dfs(i)
+        ans.append(" ".join(map(str, visited)))
     
     print("\n".join(ans))
