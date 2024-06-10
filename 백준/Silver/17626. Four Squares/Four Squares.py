e4349d6011b0c478264b10958250c64f626a05c2
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-06-10 22:38:30 +0900

    [Silver III] Title: Four Squares, Time: 60 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,23 +1,21 @@
-# Four Squares
+import math
 
-import sys
-input = sys.stdin.readline
+def fourSquares(n):
+    # √n이 정수일 때
+    if int(math.sqrt(n)) == math.sqrt(n):
+        return 1
+    # √(n - i^2)이 정수일 때
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if int(math.sqrt(n - i**2)) == math.sqrt(n - i**2):
+            return 2
+    # √(n - i^2 - j^2)이 정수일 때
+    for i in range(1, int(math.sqrt(n)) + 1):
+        for j in range(1, int(math.sqrt(n - i**2)) + 1):
+            if int(math.sqrt(n - i**2 - j**2)) == math.sqrt(n - i**2 - j**2):
+                return 3
+    # 남은 경우는 4
+    return 4
 
-def sol():
-    dp = [0] * (N + 1)
-    dp[1] = 1
 
-    for i in range(1, N + 1):
-        min_num = 1e6
-
-        for j in range(1, int(i ** 0.5) + 1):
-            min_num = min(min_num, dp[i - j ** 2])
-        
-        dp[i] = min_num + 1
-
-    return dp[N]
-
-
-if __name__ == "__main__":
-    N = int(input())
-    print(sol())
+n = int(input())
+print(fourSquares(n))
