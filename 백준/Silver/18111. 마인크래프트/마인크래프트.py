21a783782045abdd046a0272e15f622438ec6b04
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-06-11 12:09:59 +0900

    [Silver II] Title: 마인크래프트, Time: 412 ms, Memory: 121780 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -15,17 +15,16 @@ def flattening(height, inventory):
 
     # 모든 지면에 대하여 탐색.
     for i in ground:
-        gap = abs(height - i)   # 현재 기준 높이와 지면의 차.
 
         # 기준 높이보다 지면이 낮은 경우, 인벤토리에서 블럭을 꺼내 채움.
         if i < height:
-            time += gap
-            inventory -= gap
+            time += height - i
+            inventory -= height - i
 
         # 기준 높이보다 지면이 높은 경우, 블록을 제거해 인벤토리에 추가.
         elif height < i:
-            time += gap * 2
-            inventory += gap
+            time += (i - height) * 2
+            inventory += i - height
         
         # 탐색중에 이미 현재 구한 최소 시간보다 현재 시간이 길어지는 경우 종료.
         if ans_time < time:
@@ -48,15 +47,18 @@ if __name__ == "__main__":
     max_height = max(ground)
     min_height = min(ground)
 
+    # 이미 평탄화가 되어 있다면 시간에 0을 반환하고 종료.
     if max_height == min_height:
         print(0, max_height)
         exit()
 
     ans_time, ans_height = INF, 0
 
+    # 모든 높이에 대하여 탐색.
     for i in range(max_height + 1):
         result = flattening(i, B)
 
+        # 최소 시간을 갱신. 시간이 같은 경우 최대 높이만 갱신.
         if result < ans_time:
             ans_time, ans_height = result, i
         
