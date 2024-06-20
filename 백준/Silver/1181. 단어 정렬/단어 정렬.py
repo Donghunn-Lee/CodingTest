ccb693e667d097d7136bf6bf8be6115ab70e0c80
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-06-21 08:18:48 +0900

    [Silver V] Title: 단어 정렬, Time: 196 ms, Memory: 117596 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,23 @@
+# 단어 정렬
+
+import sys
+input = sys.stdin.readline
+
+def word_sort(words):
+    words.sort(key = lambda x : (len(x), x))
+    tmp = set()
+    ans = []
+
+    for i in words:
+        if i not in tmp:
+            tmp.add(i)
+            ans.append(i)
+
+    return "\n".join(ans)
+
+
+if __name__ == "__main__":
+    N = int(input())
+    words = [input().rstrip() for _ in range(N)]
+
+    print(word_sort(words))
