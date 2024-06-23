748fa24381a8b4baa10d7361292cd0988c8185bb
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-06-23 21:01:16 +0900

    [Silver V] Title: 영화감독 숌, Time: 204 ms, Memory: 110568 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,23 @@
+# 영화감독 숌
+
+import sys
+input = sys.stdin.readline
+
+def end_num(n):
+    ans = []
+    num, i = 0, 0
+
+    while num < N:
+        if '666' in str(i):
+            ans.append(i)
+            num += 1
+    
+            if num == N:
+                return ans[-1]
+        
+        i += 1
+
+
+if __name__ == "__main__":
+    N = int(input())
+    print(end_num(N))
