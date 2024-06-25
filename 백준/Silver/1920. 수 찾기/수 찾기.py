5b39a0cc7d1d11491831aa5b0543b76ac577db74
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-06-25 22:09:56 +0900

    [Silver IV] Title: 수 찾기, Time: 156 ms, Memory: 143076 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,24 @@
+# 수 찾기
+
+import sys
+input = sys.stdin.readline
+
+def find_num():
+    ans = []
+
+    for i in target:
+        if i in A:
+            ans.append('1')
+        else:
+            ans.append('0')
+    
+    return "\n".join(ans)
+
+
+if __name__ == "__main__":
+    N = int(input())
+    A = set(map(int, input().split()))
+    M = int(input())
+    target = list(map(int, input().split()))
+
+    print(find_num())
