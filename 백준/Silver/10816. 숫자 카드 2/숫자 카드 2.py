232d4cc7d6a2d253ce7007ddd66c360e23fdadd5
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-06-28 23:28:16 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 436 ms, Memory: 252232 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,30 @@
+# 숫자 카드 2
+
+import sys
+input = sys.stdin.readline
+
+def count_num():
+    ans = []
+
+    for i in target:
+        if i in deck:
+            ans.append(deck[i])
+        else:
+            ans.append(0)
+    
+    return " ".join(map(str, ans))
+
+
+if __name__ == "__main__":
+    N = int(input())
+    card_data = list(map(int, input().split()))
+    deck = dict()
+    for i in card_data:
+        if i in deck:
+            deck[i] += 1
+        else:
+            deck[i] = 1
+    M = int(input())
+    target = list(map(int, input().split()))
+
+    print(count_num())
