fa1f10070ed1cd033d64f6695ed7ed6cd38c8b9e
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-07-04 12:36:57 +0900

    [Gold II] Title: 1의 개수 세기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.py
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.py
@@ -22,6 +22,8 @@ def count_one(n):
             # 또한 2^n ~ 2^(n+1)의 수는 가장 큰 자리의 수가 모두 1이므로 그 개수도 함께 더함.
             pow = length - i - 1
             cnt += pow_sum[pow] + (n - 2 ** pow + 1)
+
+            # 이진수 왼쪽의 1을 이미 계산했으므로 주어진 수에서 1을 빼줘야 함. ex) '1'010 0010 -> 00'1'0 0010
             n = n - 2 ** pow
     
     return cnt
@@ -34,4 +36,5 @@ if __name__ == "__main__":
     for i in range(1, 60):
         pow_sum[i] = 2 ** (i - 1) + 2 * pow_sum[i - 1]
     
+    # A부터 B까지이므로 B까지의 누적합에서 A이전의 누적합을 빼줌.
     print(count_one(B) - count_one(A - 1))
