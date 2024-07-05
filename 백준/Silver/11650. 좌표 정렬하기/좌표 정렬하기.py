1ec3c1181b646a43434e130e55010e804003893f
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-07-05 20:07:37 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 292 ms, Memory: 60916 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -6,5 +6,4 @@ input = sys.stdin.readline
 if __name__ == "__main__":
     N = int(input())
     coordinate = [list(map(int, input().split())) for _ in range(N)]
-
     print("\n".join(map(lambda x : " ".join(map(str, x)), sorted(coordinate, key = lambda x : (x[0], x[1])))))
