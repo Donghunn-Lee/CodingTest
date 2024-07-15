174e73275ee277dba67b1c0e4d0a7993e2d5b40c
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-07-15 12:07:08 +0900

    [Gold II] Title: 배열 정렬, Time: 384 ms, Memory: 44744 KB -BaekjoonHub

--- 백준/Gold/28707. 배열 정렬/배열 정렬.py
+++ 백준/Gold/28707. 배열 정렬/배열 정렬.py
@@ -1,61 +1,21 @@
-# # 배열 정렬
+# 배열 정렬
 
-# # 풀릴 듯 말 듯 했는데 시간이 많이 걸릴 것 같아 검색. 2만번 후반대 문제라 자료가 많지 않았음.
-# # 우선 처음 눈에 띈 건 입력 수가 매우 적은 것. 배열의 길이가 8이하이고 조작의 개수도 10개라 복잡한 연산의 필요를 느낌.
-# # 완전탐색으로 각각의 배열 값에 코스트를 묶어서 계산해야 하는 것까진 생각했으나, 쉽사리 다익스트라를 연관짓지는 못함.
-# # 다익스트라임가 필요함을 확인 후, 조작 후 배열의 값을 하나의 노드로 하여 코스트를 최소화 하는 방법까지 떠올림.
-# # 검색한 코드를 참고하여 코드 작성. 검색한 문제에선 클래스를 사용하여 노드를 구현했는데, 나는 그럴 필요를 느끼지 못함.
-# # 그러나 바꾸고 나니 visited의 추가, list변환과 tuple 변환을 계속해가며 시간이 많이 길어질 수도 있겠다고 생각함.
+# 풀릴 듯 말 듯 했는데 시간이 많이 걸릴 것 같아 검색. 2만번 후반대 문제라 자료가 많지 않았음.
+# 우선 처음 눈에 띈 건 입력 수가 매우 적은 것. 배열의 길이가 8이하이고 조작의 개수도 10개라 복잡한 연산의 필요를 느낌.
+# 완전탐색으로 각각의 배열 값에 코스트를 묶어서 계산해야 하는 것까진 생각했으나, 쉽사리 다익스트라를 연관짓지는 못함.
+# 다익스트라임가 필요함을 확인 후, 조작 후 배열의 값을 하나의 노드로 하여 코스트를 최소화 하는 방법까지 떠올림.
+# 검색한 코드를 참고하여 코드 작성. 검색한 문제에선 클래스를 사용하여 노드를 구현했는데, 나는 그럴 필요를 느끼지 못함.
 
-# import sys, heapq
-# input = sys.stdin.readline
-
-# def sol(seq):
-#     graph = dict()
-#     graph[seq] = 0
-#     q = []
-#     heapq.heappush(q, (graph[seq], seq))
-#     visited = set()
-
-#     while q:
-#         cost, cur_seq = heapq.heappop(q)
-
-#         if cur_seq in visited:
-#             continue
-
-#         for l, r, c in operation:
-#             nxt_seq = list(cur_seq)
-#             nxt_seq[l], nxt_seq[r] = nxt_seq[r], nxt_seq[l]
-#             nxt_seq = tuple(nxt_seq)
-#             nxt_cost = cost + c
-        
-#             if nxt_seq not in graph:
-#                 graph[nxt_seq] = nxt_cost
-#                 heapq.heappush(q, (nxt_cost, nxt_seq))
-                
-#             elif nxt_cost < graph[nxt_seq]:
-#                 graph[nxt_seq] = nxt_cost
-#                 heapq.heappush(q, (nxt_cost, nxt_seq))
-        
-#         visited.add(cur_seq)
-    
-#     return graph
-
-
-# if __name__ == "__main__":
-#     N = int(input())
-#     A = tuple([0] + list(map(int, input().split())))
-#     M = int(input())
-#     operation = [tuple(map(int, input().split())) for _ in range(M)]
-
-#     target_seq = tuple(sorted(A))
-
-#     graph = sol(A)
-
-#     print(graph[target_seq] if graph.get(target_seq) else -1)
+# 다 된 것 같은데 결국 내 코드는 풀리지 않았음. 검색한 코드를 게시자가 완전히 올리지 않아서 부족한 부분을 찾아 수정.
+# 통과된 정답 코드와 내 코드의 차이점은, 클래스를 생성해 배열을 키로 하는 딕셔너리에 저장할 때 visited의 유무였음.
+# 나는 현재 while에서 탐색중인 배열의 키 값만을 visited에 추가, graph내의 키값의 유무에 따라 값을 추가함.
+# 클래스를 사용한 정답코드에선 현재 탐색한 배열을 방문처리하는 것은 같으나, 키값이 아닌 클래스 내부의 visited로 체크.
+# 이렇게 하니 정답 배열을 찾는 특정 시점에서 내 코드는 graph길이가 15, 정답코드는 17개였음.
+# 애초에 난 틀렸고 이건 맞으니 다른 점은 있을텐데, 사실 아직도 정확히 파악하진 못함.
+# 클래스라 해도 결국 노드 하나 당 하나의 visited 값을 가지는 셈이고, 새 노드를 찾았을 때 graph에 추가하는 것도 같은데.
+# 일단 시간이 너무 많이 들어 이쯤 하고 넘어가는 걸로. 나중에 다시 제대로 원인을 파악할 수 있으면 좋겠음.
 
 import sys, heapq
-
 input = sys.stdin.readline
 
 class Node:
