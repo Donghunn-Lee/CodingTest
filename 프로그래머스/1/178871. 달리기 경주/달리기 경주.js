18e5a84904cb06149ca93f2668d0702248c9976f
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-08-02 10:56:29 +0900

    [level 1] Title: 달리기 경주, Time: 197.80 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,19 @@
+function solution(players, callings) {
+    let players_dict = {};
+    players.forEach((e, idx) => players_dict[e] = idx);
+    
+    for (const name of callings) {
+        let tmp = players[players_dict[name] - 1];
+        let tmp_dict = players_dict[name];
+
+        players[players_dict[name] - 1] = name;
+        players[players_dict[name]] = tmp;
+        players_dict[name] -= 1;
+        players_dict[tmp] += 1;
+        
+    }
+    
+    return players;
+}
+
+console.log(solution(	["mumu", "soe", "poe", "kai", "mine"], ["kai", "kai", "mine", "mine"]))
