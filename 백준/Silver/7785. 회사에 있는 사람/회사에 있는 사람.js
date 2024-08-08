b0355c892be667a09b77597cca291bdc87b88cc2
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-08-08 10:46:56 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 352 ms, Memory: 40832 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.js
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.js
@@ -1,35 +1,19 @@
-// 회사에 있는 사람
-
+// dev/stdin
 const fs = require("fs");
+let input = fs.readFileSync('dev/stdin').toString().trim().split('\n');
 
-function Input() {
-    return fs.readFileSync(process.platform === "linux" ? "/dev/stdin" : "./baekjoon/JS/input.txt")
-    .toString()
-    .trim()
-    .split("\n")
+const n = Number(input[0])
+
+let set = new Set()
+
+for(let i = 1; i <= n; i++) {
+  const temp = input[i].split(' ')
+  if(temp[1].includes('enter')) {
+    set.add(temp[0])
+  } else {
+    set.delete(temp[0])
+  }
 }
+const a = 'a'.toLocaleLowerCase
 
-function sol() {
-    const inputData = Input();
-    const N = inputData.shift();
-    const log = inputData.map((e) => e.split(" ").map((e) => e.trim()));
-    const inCompany = new Set();
-
-    for (let i = 0; i < N; i++) {
-        if (log[i][1] === 'enter') {
-            inCompany.add(log[i][0]);
-        } else {
-            inCompany.delete(log[i][0]);
-        }
-    }
-
-    return [...inCompany].sort((a, b) => {
-        if (a < b) {
-            return 1;
-        } else {
-            return -1;
-        }
-    }).join("\n");
-}
-
-console.log(sol());
+console.log([...set].sort().reverse().join('\n'))
