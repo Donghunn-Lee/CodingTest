2722a4d5ed0bab0ebf53ba820b8f1f89a0b58e3b
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-08-10 12:09:33 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.54 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -9,18 +9,14 @@ function solution(today, terms, privacies) {
     privacies.forEach((val, idx, arr) => arr[idx] = val.split(" "))
     
     for (let i = 0; i < N; i++) {
-        let date = privacies[i][0].split(".").map(Number);
-        let grade = privacies[i][1];
+        let [date, grade] = [privacies[i][0].split(".").map(Number), privacies[i][1]];
         let elapsedMonth = 0;
         
-        elapsedMonth = (today[0] - date[0]) * 12;
-        elapsedMonth += today[1] - date[1];
-        elapsedMonth += today[2] - date[2] < 0 ? -1 : 0;
+        elapsedMonth = ((today[0] - date[0]) * 12) + (today[1] - date[1]) + (today[2] - date[2] < 0 ? -1 : 0);
         
         if (terms_dict[grade] <= elapsedMonth) {
             answer.push(i + 1)
         }
-        
     }
     
     return answer;
