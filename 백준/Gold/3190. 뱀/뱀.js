3650918d4084df8224add0fcd2819c2ecc0cff62
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-08-13 11:08:15 +0900

    [Gold IV] Title: 뱀, Time: 124 ms, Memory: 10392 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.js
+++ 백준/Gold/3190. 뱀/뱀.js
@@ -1,76 +1,65 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// 뱀
 
-const input = [];
-// deque
-const snakeBody = [[0, 0]];
-// 상우하좌 (시계방향)
-const dy = [-1, 0, 1, 0];
-const dx = [0, 1, 0, -1];
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
 
-// 컨트롤러 변수
-let currentDirection = 1; // 기본값 우방향
-let seconds = 0;
-let isApple;
-let collision = false;
+function sol() {
+  const N = +input.shift();
+  const K = +input.shift();
+  const board = Array(N + 1)
+    .fill()
+    .map(() => Array(N + 1).fill(0));
+  board[1][1] = 1;
+  const apples = input.slice(0, K).map((e) => e.split(" ").map(Number));
+  const L = +input.shift();
+  const directionChange = {};
+  input
+    .slice(K)
+    .map((e) => e.split(" "))
+    .forEach((e) => (directionChange[+e[0]] = e[1]));
 
-// L or D를 받아서 전역 현재 방향 변수를 0 1 2 3 중 하나로 한 칸 바꿈
-const turn = (LD) => {
-  if (LD === "L")
-    currentDirection = currentDirection ? currentDirection - 1 : 3;
-  if (LD === "D")
-    currentDirection = currentDirection === 3 ? 0 : currentDirection + 1;
-};
+  const snake = [[1, 1]];
+  const di = [0, 1, 0, -1];
+  const dj = [1, 0, -1, 0];
+  let direction = 0;
+  let time = 0;
 
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  const N = +input[0];
-  const appleNum = +input[1];
-  let applePosList = input
-    .slice(2, 2 + appleNum)
-    .map((el) => el.split(" ").map(Number));
-  const directionNum = +input[appleNum + 2];
-  const directionList = input.slice(appleNum + 3).map((el) => {
-    const directionInfo = el.split(" ");
-    return [+directionInfo[0], directionInfo[1]];
-  });
+  for (const apple of apples) {
+    board[apple[0]][apple[1]] = "a";
+  }
+
   while (true) {
-    isApple = false;
-    const newY = snakeBody[0][0] + dy[currentDirection];
-    const newX = snakeBody[0][1] + dx[currentDirection];
-    // 벽 충돌 검사
-    if (newY < 0 || newY >= N || newX < 0 || newX >= N) break;
-    // 몸체 충돌 검사
-    snakeBody.forEach((body) => {
-      if (body[0] === newY && body[1] === newX) {
-        collision = true;
+    time++;
+
+    const head = snake.at(-1);
+    let ni = head[0] + di[direction];
+    let nj = head[1] + dj[direction];
+
+    if (1 <= ni && ni <= N && 1 <= nj && nj <= N && board[ni][nj] !== 1) {
+      snake.push([ni, nj]);
+
+      if (board[ni][nj] !== "a") {
+        let tail = snake.shift();
+        board[tail[0]][tail[1]] = 0;
       }
-    });
-    if (collision) break;
-    // 사과 검사
-    // 사과인 경우 pop X, unshift O
-    applePosList.forEach((applePos, idx) => {
-      if (applePos[0] - 1 === newY && applePos[1] - 1 === newX) {
-        applePosList.splice(idx, 1);
-        snakeBody.unshift([newY, newX]);
-        isApple = true;
-      }
-    });
-    // 사과가 아닌 경우 pop O
-    if (!isApple) {
-      snakeBody.unshift([newY, newX]);
-      snakeBody.pop();
+
+      board[ni][nj] = 1;
+    } else {
+      return time;
     }
-    seconds++;
-    // 방향 전환
-    if (directionList.length && seconds === directionList[0][0]) {
-      turn(directionList[0][1]);
-      directionList.shift();
+
+    if (directionChange[time]) {
+      if (directionChange[time] == "D") {
+        direction = (direction + 1) % 4;
+      } else {
+        direction = direction - 1 < 0 ? 4 - direction - 1 : direction - 1;
+      }
     }
   }
-  console.log(seconds + 1);
-});
+}
+
+console.log(sol());
