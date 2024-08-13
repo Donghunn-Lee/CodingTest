364c305e5136b32d77e3a23a402239822ff40b1e
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-08-13 15:26:01 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 236 ms, Memory: 19704 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.js
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.js
@@ -7,24 +7,33 @@ const input = fs.readFileSync(process.platform === "linux" ? "/dev/stdin" : "./i
 
 
 function sol() {
-    const [N, M] = input.shift().split(" ").map(Number);
-    const arr = input.slice(0, N).map(e => e.split(" ").map(Number));
-    const K = +input[N];
-    const area = input.slice(N + 1);
+    const [N, M] = input[0].split(" ").map(Number);
+    const arr = input.slice(1, N + 1).map(e => e.split(" ").map(Number));
+    const areas = input.slice(N + 2).map(e => e.split(" ").map(Number));
+    const sum = Array(N + 1).fill().map(_ => Array(M + 1).fill(0));
     const answer = [];
 
-    for (let k = 0; k < K; k++) {
-        const [y1, x1, y2, x2] = area[k].split(" ").map(Number);
-        let sum = 0;
-
-        for (let i = y1; i <= y2; i++) {
-            for (let j = x1; j <= x2; j++) {
-                sum += arr[i - 1][j - 1];
-            }
-        }
-
-        answer.push(sum);
+    for (let i = 0; i < N; i++) {
+        sum[i + 1][1] = sum[i][1] + arr[i][0];
+        
     }
+
+    for (let j = 0; j < M; j++) {
+        sum[1][j + 1] = sum[1][j] + arr[0][j];
+    }
+
+    for (let i = 2; i <= N; i++) {
+        for (let j = 2; j <= M; j++) {
+            sum[i][j] = arr[i - 1][j - 1] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+        }
+    }
+
+
+    for (let area of areas) {
+        const [si, sj, ei, ej] = area;
+        answer.push(sum[ei][ej] - sum[si-1][ej] - sum[ei][sj-1] + sum[si-1][sj-1]);
+    }
+
 
     return answer.join("\n");
 }
