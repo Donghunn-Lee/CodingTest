c4cbc0db4d56814bd19a34d734f82db668b2cab7
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-08-17 20:09:04 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,10 @@
+function solution(food) {
+    var answer = '';
+    
+    for (let i = 1; i < food.length; i++) {
+        let num = Math.floor(food[i] / 2);
+        answer += i.toString().repeat(num);
+    }
+    
+    return answer + '0' + [...answer].reverse().join("");
+}
