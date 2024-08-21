9de83632602fca22406692f26bdf5eee6e6065c7
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-08-21 15:36:08 +0900

    [level 1] Title: 삼총사, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -4,8 +4,6 @@ function solution(number) {
     for (let i = 0; i < number.length; i++) {
         for (let j = i + 1; j < number.length; j++) {
             for (let k = j + 1; k < number.length; k++) {
-                if (i === j || j === k || i === k) continue;
-                
                 if (number[i] + number[j] + number[k] === 0) {
                     answer++;
                 }
