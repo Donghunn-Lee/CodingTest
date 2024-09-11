b8713f592795f5b3833eca549ceb8ab6e5c91124
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-09-11 21:37:10 +0900

    [Silver V] Title: 타일 장식물, Time: 100 ms, Memory: 9336 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13301. 타일 장식물/타일 장식물.js
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.js
@@ -1,26 +1,26 @@
 // 타일 장식물
 
 function dp(n) {
-    const dp_table = Array(n).fill(0);
-    dp_table[0] = 1;
-    dp_table[1] = 1;
+    const dp_table = Array(n + 1).fill(BigInt(0));
+    dp_table[1] = BigInt(1);
+    dp_table[2] = BigInt(1);
 
     if (n === 1) {
         return 4;
     }
 
-    for (let i = 2; i < n; i++) {
+    for (let i = 3; i <= n; i++) {
         dp_table[i] = dp_table[i - 1] + dp_table[i - 2];
     }
 
-    return dp_table[n - 1] * 4 + dp_table[n - 2] * 2;
+    return dp_table[n] * BigInt(4) + dp_table[n - 1] * BigInt(2);
 }
 
 function main() {
     const fs = require('fs');
     const N = +fs.readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim();
 
-    console.log(dp(N));
+    console.log(dp(N).toString());
 }
 
 main();
