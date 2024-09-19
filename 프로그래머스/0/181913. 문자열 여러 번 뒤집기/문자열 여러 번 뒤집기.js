927db6bf0af78f96f594811cc0df32bbc970210d
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-09-19 22:27:53 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 4.32 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -1,9 +1,11 @@
-function solution(my_string, queries) {
+function solution(str, queries) {
+    str = [...str];
+    
     for (let q of queries) {
         let [s, e] = q;
         
-        my_string = my_string.slice(0, s) + [...my_string.slice(s, e + 1)].reverse().join("") + my_string.slice(e + 1);
+        str.splice(s, e - s + 1, ...str.slice(s, e + 1).reverse())
     }
     
-    return my_string;
+    return str.join("");
 }
