7ea25b6e11f227f63ad4973c662aba41ad9300e0
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-10-06 23:32:52 +0900

    [Bronze I] Title: 2007년, Time: 92 ms, Memory: 9392 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const input = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `1 1`
+).split(" ");
+
+const date = new Date(`2007-${input[0]}-${input[1]}`);
+
+// solution 1
+const day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+// solution 2
+console.log(date.toString().slice(0, 3).toUpperCase());
