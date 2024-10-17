d3ea462bba79af22aa8c31175ddbcec25f77be98
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2024-10-17 22:20:38 +0900

    [Silver I] Title: 포닉스의 문단속, Time: 180 ms, Memory: 18728 KB -BaekjoonHub

--- 백준/Silver/31784. 포닉스의 문단속/포닉스의 문단속.js
+++ 백준/Silver/31784. 포닉스의 문단속/포닉스의 문단속.js
@@ -6,6 +6,7 @@
 // 왼쪽부터 문자를 탐색하며 남은 K번으로 이 문자를 A로 만들 수 있는지를 계속해서 체크. 가능하면 변환.
 // 모든 문자를 탐색하고 K가 0 이상인 경우, 사전 순으로 가장 작아야 하므로 마지막 문자를 K번 더 돌림.
 
+// + 이미 문자가 A일 때 돌릴 필요가 없다는 것과, 마지막에 남은 K가 26보다 큰 경우 K를 버리는 게 아니라 남은 횟수만큼 더 돌려야 한다는 점을 생각하지 못함. 떠올려서 남은 K를 26으로 mod연산함.
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = require('fs')
   .readFileSync(filePath)
