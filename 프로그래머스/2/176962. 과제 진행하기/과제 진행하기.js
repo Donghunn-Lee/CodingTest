0405a37217985aa3b818bdc93501cac6cf3599d3
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2025-07-30 00:32:29 +0900

    [level 2] Title: 과제 진행하기, Time: 3.05 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
@@ -7,10 +7,8 @@ function solution(plans) {
   const answer = [];
   const stack = [];
 
-  // 시간 정수화
   plans = plans.map(([name, time, duration]) => [name, timeToMinute(time), +duration]);
 
-  // 시작 시간 기준 정렬
   plans.sort((a, b) => a[1] - b[1]);
 
   for (let i = 0; i < plans.length - 1; i++) {
@@ -19,11 +17,9 @@ function solution(plans) {
     let gap = nextStart - start;
 
     if (gap >= duration) {
-      // 현재 과제를 다 끝냄
       answer.push(name);
       gap -= duration;
 
-      // 그 남은 시간 동안 스택 처리
       while (gap > 0 && stack.length > 0) {
         const top = stack[stack.length - 1];
         if (top[1] <= gap) {
@@ -35,15 +31,12 @@ function solution(plans) {
         }
       }
     } else {
-      // 다 못 끝내고 중단
       stack.push([name, duration - gap]);
     }
   }
 
-  // 마지막 과제는 무조건 완료됨
   answer.push(plans.at(-1)[0]);
 
-  // 남은 스택 처리
   while (stack.length > 0) {
     answer.push(stack.pop()[0]);
   }
