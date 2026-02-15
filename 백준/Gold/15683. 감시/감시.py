630f54f88e1533b8afedca1fc43a111b4c14acc6
Ethan <93193485+Donghunn-Lee@users.noreply.github.com>
2026-02-15 16:56:20 +0900

    [Gold III] Title: 감시, Time: 212 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,84 +1,71 @@
-import sys, copy
+# 15683 감시
+
+# js로는 예전에 풀었었고 어렴풋이 기억은 나는 문제
+# 다만 파이썬으로 하려니 역시 기본문법이 퇴화해서 쉽지 않았음
+# 생각없이 deepcopy 써서 풀다가, 백트래킹을 뒤늦게 생각했지만
+# 방법이 헷갈려 지피티에게 문의
+
+import sys
 input = sys.stdin.readline
 
-def findCCTV():
-    cctvs = []
-    for i in range(N):
-        for j in range(M):
-            if 1 <= graph[i][j] <= 5:
-                cctvs.append([i, j, graph[i][j]])
-    return cctvs
+# 상, 우, 하, 좌
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-def monitoring(board, si, sj, d):
-    ni, nj = si, sj
+# CCTV 타입별 가능한 방향 조합
+cctv_dirs = {
+    1: [[0], [1], [2], [3]],
+    2: [[0, 2], [1, 3]],
+    3: [[0, 1], [1, 2], [2, 3], [3, 0]],
+    4: [[0, 1, 2], [1, 2, 3], [2, 3, 0], [3, 0, 1]],
+    5: [[0, 1, 2, 3]]
+}
+
+def watch(x, y, d, changed):
+    nx, ny = x, y
     while True:
-        ni += dy[d]
-        nj += dx[d]
-
-        if ni < 0 or ni >= N or nj < 0 or nj >= M:
+        nx += dx[d]
+        ny += dy[d]
+        if nx < 0 or nx >= N or ny < 0 or ny >= M or board[nx][ny] == 6:
             break
-        if board[ni][nj] == 6:
-            break
-        if board[ni][nj] == 0:
-            board[ni][nj] = -1
+        if board[nx][ny] == 0:
+            board[nx][ny] = -1
+            changed.append((nx, ny))
 
-def countBlind(board):
-    cnt = 0
-    for i in range(N):
-        cnt += board[i].count(0)
-    return cnt
-
-def dfs(cur_graph, cctvNum):
+def dfs(idx):
     global answer
 
-    if cctvNum == len(cctvs):
-        answer = min(answer, countBlind(cur_graph))
+    if idx == len(cctvs):
+        cnt = 0
+        for i in range(N):
+            cnt += board[i].count(0)
+        answer = min(answer, cnt)
         return
 
-    ci, cj, t = cctvs[cctvNum]
+    x, y, t = cctvs[idx]
 
-    # type 1,3,4 → 4방향 회전
-    if t in [1, 3, 4]:
-        for i in range(4):
-            nxt_graph = copy.deepcopy(cur_graph)
-            for d in cctv_types[t]:
-                monitoring(nxt_graph, ci, cj, (d + i) % 4)
-            dfs(nxt_graph, cctvNum + 1)
+    for dirs in cctv_dirs[t]:
+        changed = []
+        for d in dirs:
+            watch(x, y, d, changed)
 
-    # type 2 → 2방향
-    elif t == 2:
-        for i in range(2):
-            nxt_graph = copy.deepcopy(cur_graph)
-            for d in cctv_types[t]:
-                monitoring(nxt_graph, ci, cj, (d + i) % 4)
-            dfs(nxt_graph, cctvNum + 1)
+        dfs(idx + 1)
 
-    # type 5 → 고정
-    else:
-        nxt_graph = copy.deepcopy(cur_graph)
-        for d in cctv_types[t]:
-            monitoring(nxt_graph, ci, cj, d)
-        dfs(nxt_graph, cctvNum + 1)
+        # 원복
+        for cx, cy in changed:
+            board[cx][cy] = 0
 
 
-# main
+# 입력
 N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
+board = [list(map(int, input().split())) for _ in range(N)]
 
-dy = [-1, 0, 1, 0]
-dx = [0, 1, 0, -1]
+cctvs = []
+for i in range(N):
+    for j in range(M):
+        if 1 <= board[i][j] <= 5:
+            cctvs.append((i, j, board[i][j]))
 
-cctv_types = [
-    [],
-    [0],          # 1
-    [0, 2],       # 2
-    [0, 1],       # 3
-    [0, 1, 2],    # 4
-    [0, 1, 2, 3]  # 5
-]
-
-cctvs = findCCTV()
 answer = N * M
-
-dfs(graph, 0)
+dfs(0)
 print(answer)
